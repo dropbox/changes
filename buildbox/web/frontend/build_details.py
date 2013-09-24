daf04ddad841989d1fde7aa69326d6651c9bad95
David Cramer <dcramer@gmail.com>
2013-09-23 21:53:47 -0700

    Add test result mock data to build details and design overhaul

--- buildbox/web/frontend/build_details.py
+++ buildbox/web/frontend/build_details.py
@@ -1,6 +1,8 @@
 from collections import defaultdict
 
-from buildbox.models import Build, Phase, Step, Author, Revision
+from buildbox.models import (
+    Build, Phase, Step, Test, Revision, Author
+)
 
 from buildbox.web.base_handler import BaseRequestHandler
 
@@ -9,8 +11,9 @@ class BuildDetailsHandler(BaseRequestHandler):
     def get(self, project_id, build_id):
         with self.db.get_session() as session:
             build = session.query(Build).get(build_id)
-            phases = list(session.query(Phase).filter_by(build_id=build.id))
+            phase_list = list(session.query(Phase).filter_by(build_id=build.id))
             steps = list(session.query(Step).filter_by(build_id=build.id))
+            test_list = list(session.query(Test).filter_by(build_id=build.id))
 
             # TODO: probably a better way to do relations
             revision = session.query(Revision).filter_by(
@@ -21,14 +24,15 @@ class BuildDetailsHandler(BaseRequestHandler):
         for step in steps:
             steps_by_phase[step.phase_id].append(step)
 
-        for phase in phases:
+        for phase in phase_list:
             phase.steps = steps_by_phase[phase.id]
 
         context = {
             'build': build,
             'revision': revision,
             'author': author,
-            'phases': phases,
+            'phase_list': phase_list,
+            'test_list': test_list,
         }
 
         return self.render("build_details.html", **context)
