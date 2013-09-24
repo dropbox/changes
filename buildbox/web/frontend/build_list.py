f61a4cd174c677043469ee854a4636ca53173a30
David Cramer <dcramer@gmail.com>
2013-09-24 14:59:40 -0700

    Add author details to build list

--- buildbox/web/frontend/build_list.py
+++ buildbox/web/frontend/build_list.py
@@ -1,4 +1,4 @@
-from buildbox.models import Build, Project
+from buildbox.models import Build, Project, Author
 
 from buildbox.web.base_handler import BaseRequestHandler
 
@@ -6,7 +6,7 @@ from buildbox.web.base_handler import BaseRequestHandler
 class BuildListHandler(BaseRequestHandler):
     def get(self):
         with self.db.get_session() as session:
-            build_list = session.query(Build).join(Project)
+            build_list = session.query(Build).join(Project).outerjoin(Author)
 
         context = {
             'build_list': build_list,
