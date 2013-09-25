325d6d7b074b2b34db38dd1324f9a64c5b40676b
David Cramer <dcramer@gmail.com>
2013-09-24 23:55:05 -0700

    Dont load initial data in BuildListHandler

--- buildbox/web/frontend/build_list.py
+++ buildbox/web/frontend/build_list.py
@@ -1,21 +1,6 @@
-from sqlalchemy.orm import joinedload
-
-from buildbox.models import Build
 from buildbox.web.base_handler import BaseRequestHandler
 
 
 class BuildListHandler(BaseRequestHandler):
     def get(self):
-        with self.db.get_session() as session:
-            build_list = list(
-                session.query(Build).options(
-                    joinedload(Build.project),
-                    joinedload(Build.author),
-                ).order_by(Build.date_created.desc())
-            )[:100]
-
-        context = {
-            'build_list': build_list,
-        }
-
-        return self.render("build_list.html", **context)
+        return self.render("build_list.html")
