3320440e89931f7658a81ac9b671d80557ce5f6f
David Cramer <dcramer@gmail.com>
2013-10-01 13:02:25 -0700

    Add MockHTTPConnection to testutils

--- changes/testutils/http.py
+++ changes/testutils/http.py
@@ -1,6 +1,8 @@
 import json
 import os.path
 
+from cStringIO import StringIO
+
 
 class MockedResponse(object):
     # used to mock out KoalityBackend._get_response
@@ -25,3 +27,29 @@ class MockedResponse(object):
     def url_to_filename(self, url):
         assert url.startswith(self.base_url)
         return url[len(self.base_url) + 1:].strip('/').replace('/', '__') + '.json'
+
+
+class MockedHTTPConnection(object):
+    def __init__(self, *args, **kwargs):
+        self.responder = MockedResponse(*args, **kwargs)
+
+    def __call__(self, host, **kwargs):
+        """
+        Acts as HTTPConnection.__init__
+        """
+        self._host = host
+        self._request = None
+        return self
+
+    def request(self, method, path, body=None, headers={}):
+        assert self._request is None
+        self._request = {
+            'method': method,
+            'url': 'http://{0}/{1}'.format(self._host, path),
+            'data': body,
+            'headers': headers,
+        }
+
+    def getresponse(self):
+        assert self._request is not None
+        return StringIO(json.dumps(self.responder(**self._request)))
