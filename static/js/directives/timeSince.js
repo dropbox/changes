cb467a6dfaccbdac8c0843b3a858466ba8d8ab59
David Cramer <dcramer@gmail.com>
2013-10-21 13:44:21 -0700

    Automatically update timeSince values

--- static/js/directives/timeSince.js
+++ static/js/directives/timeSince.js
@@ -1,10 +1,19 @@
 define(['app', 'moment'], function(app, moment) {
-  app.directive('timeSince', function() {
+  app.directive('timeSince', ['$timeout', function($timeout) {
     return function timeSince(scope, element, attrs) {
       var $element = $(element);
-      scope.$watch(attrs.timeSince, function(value) {
+
+      function tick(){
+      	value = scope.$eval(attrs.timeSince);
         element.text(moment.utc(value).fromNow());
+        $timeout(tick, 500);
+      }
+
+      scope.$watch(attrs.timeSince, function(value){
+      	element.text(moment.utc(value).fromNow());
       });
+
+      tick();
     }
-  });
+  }]);
 });
