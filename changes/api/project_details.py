b0aba578b8bcb72165550c4b1f2f5addf9017dbe
David Cramer <dcramer@gmail.com>
2013-10-24 18:03:03 -0700

    Add update project endpoint

--- changes/api/project_details.py
+++ changes/api/project_details.py
@@ -1,8 +1,42 @@
-from flask import Response
+from flask import Response, request
 from sqlalchemy.orm import joinedload
 
 from changes.api.base import APIView
+from changes.config import db
 from changes.models import Project
+
+
+class ValidationError(Exception):
+    pass
+
+
+class Validator(object):
+    fields = ()
+
+    def __init__(self, data=None, initial=None):
+        self.data = data or {}
+        self.initial = initial or {}
+
+    def clean(self):
+        result = {}
+        for name in self.fields:
+            value = self.data.get(name, self.initial.get(name))
+            if isinstance(value, basestring):
+                value = value.strip()
+            result[name] = value
+
+        for key, value in result.iteritems():
+            if not value:
+                raise ValidationError('%s is required' % (key,))
+
+        return result
+
+
+class ProjectValidator(Validator):
+    fields = (
+        'name',
+        'slug',
+    )
 
 
 class ProjectDetailsAPIView(APIView):
@@ -12,6 +46,35 @@ class ProjectDetailsAPIView(APIView):
         ).get(project_id)
         if project is None:
             return Response(status=404)
+
+        context = {
+            'project': project,
+        }
+
+        return self.respond(context)
+
+    def post(self, project_id):
+        project = Project.query.options(
+            joinedload(Project.repository),
+        ).get(project_id)
+        if project is None:
+            return Response(status=404)
+
+        validator = ProjectValidator(
+            data=request.form,
+            initial={
+                'name': project.name,
+                'slug': project.slug,
+            },
+        )
+        try:
+            result = validator.clean()
+        except ValidationError:
+            return Response(status=400)
+
+        project.name = result['name']
+        project.slug = result['slug']
+        db.session.add(project)
 
         context = {
             'project': project,
--- tests/changes/api/test_project_details.py
+++ tests/changes/api/test_project_details.py
@@ -1,8 +1,9 @@
+from changes.models import Project
 from changes.testutils import APITestCase
 
 
 class ProjectDetailsTest(APITestCase):
-    def test_simple(self):
+    def test_retrieve(self):
         path = '/api/0/projects/{0}/'.format(
             self.project.id.hex)
 
@@ -10,3 +11,17 @@ class ProjectDetailsTest(APITestCase):
         assert resp.status_code == 200
         data = self.unserialize(resp)
         assert data['project']['id'] == self.project.id.hex
+
+    def test_Update(self):
+        path = '/api/0/projects/{0}/'.format(
+            self.project.id.hex)
+
+        resp = self.client.post(path, data={
+            'name': 'details test project',
+            'slug': 'details-test-project',
+        })
+        assert resp.status_code == 200
+
+        project = Project.query.get(self.project.id)
+        assert project.name == 'details test project'
+        assert project.slug == 'details-test-project'
