c89d5b4afc2c7981b2b3ca35bdc5a121e27818bc
David Cramer <dcramer@gmail.com>
2013-11-04 14:42:54 -0800

    Add sortArray helper

---
+++ static/js/utils/sortArray.js
@@ -0,0 +1,22 @@
+define([], function() {
+  'use strict';
+
+  return function sortArray(arr, score_fn) {
+    arr.sort(function(a, b){
+      var a_score = score_fn(a),
+          b_score = score_fn(b);
+
+      for (var i = 0; i < a_score.length; i++) {
+        if (a_score[i] < b_score[i]) {
+          return 1;
+        }
+        if (a_score[i] > b_score[i]) {
+          return -1
+        }
+      }
+      return 0;
+    });
+
+    return arr;
+  }
+});
