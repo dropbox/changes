d827e2760f7c5747d089cca18b8fae1a439c1ade
David Cramer <dcramer@gmail.com>
2013-11-06 16:22:57 -0800

    Add raven.js

--- changes/web/index.py
+++ changes/web/index.py
@@ -1,7 +1,22 @@
-from flask import render_template
+import urlparse
+
+from flask import render_template, current_app
 from changes.api.base import MethodView
 
 
 class IndexView(MethodView):
     def get(self, path=''):
-        return render_template('index.html')
+        if current_app.config['SENTRY_DSN']:
+            parsed = urlparse.urlparse(current_app.config['SENTRY_DSN'])
+            dsn = '%s://%s@%s/%s' % (
+                parsed.scheme,
+                parsed.username,
+                parsed.hostname + (':%s' % (parsed.port,) if parsed.port else ''),
+                parsed.path,
+            )
+        else:
+            dsn = None
+
+        return render_template('index.html', **{
+            'SENTRY_PUBLIC_DSN': dsn,
+        })
