23212ac24647fc856b008d9aecdf81c121577d7f
David Cramer <dcramer@gmail.com>
2013-11-07 13:56:59 -0800

    Test behavior of num_failed

--- tests/changes/models/test_test.py
+++ tests/changes/models/test_test.py
@@ -11,8 +11,8 @@ class TestResultTestCase(TestCase):
             suite_name='foobar',
             name='Test',
             package='tests.changes.handlers.test_xunit',
-            result=Result.skipped,
-            message='collection skipped',
+            result=Result.failed,
+            message='collection failed',
             duration=156,
         )
         test = result.save()
@@ -21,8 +21,8 @@ class TestResultTestCase(TestCase):
         assert test.project == self.project
         assert test.name == 'Test'
         assert test.package == 'tests.changes.handlers.test_xunit'
-        assert test.result == Result.skipped
-        assert test.message == 'collection skipped'
+        assert test.result == Result.failed
+        assert test.message == 'collection failed'
         assert test.duration == 156
 
         suite = test.suite
@@ -39,13 +39,13 @@ class TestResultTestCase(TestCase):
         assert groups[0].name == 'tests.changes.handlers.test_xunit'
         assert groups[0].duration == 156
         assert groups[0].num_tests == 1
-        assert groups[0].num_failed == 0
-        assert groups[0].result == Result.skipped
+        assert groups[0].num_failed == 1
+        assert groups[0].result == Result.failed
 
         assert groups[1].build == build
         assert groups[1].project == self.project
         assert groups[1].name == 'tests.changes.handlers.test_xunit.Test'
         assert groups[1].duration == 156
         assert groups[1].num_tests == 1
-        assert groups[1].num_failed == 0
-        assert groups[1].result == Result.skipped
+        assert groups[1].num_failed == 1
+        assert groups[1].result == Result.failed
