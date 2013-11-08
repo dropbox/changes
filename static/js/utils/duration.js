e09f307612ddd339630b70af8b023fd5dada563b
David Cramer <dcramer@gmail.com>
2013-11-07 17:08:23 -0800

    Add duration to project build list

--- static/js/utils/duration.js
+++ static/js/utils/duration.js
@@ -7,13 +7,15 @@ define(['app'], function(app) {
     var result;
 
     if (value > 7200000) {
-      result = round(value / 3600000) + ' hr';
+      result = Math.round(value / 3600000) + ' hr';
     } else if (value > 120000) {
-      result = round(value / 60000) + ' min';
+      result = Math.round(value / 60000) + ' min';
+    } else if (value > 10000) {
+      result = Math.round(value / 1000) + ' sec';
     } else if (value > 1000) {
       result = round(value / 1000) + ' sec';
     } else {
-      result = parseInt(value, 10) + ' ms';
+      result = Math.round(value) + ' ms';
     }
 
     return result;
