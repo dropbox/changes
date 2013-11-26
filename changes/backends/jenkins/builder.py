fe30c28beb2f59e79f6fec9382125e13e980b630
David Cramer <dcramer@gmail.com>
2013-11-26 11:39:46 -0800

    When chunking try to ensure newline termination
    Reviewers: cf
    
    Differential Revision: https://tails.corp.dropbox.com/D29414

--- changes/backends/jenkins/builder.py
+++ changes/backends/jenkins/builder.py
@@ -17,12 +17,25 @@ from changes.models import RemoteEntity, TestResult, LogSource, LogChunk
 
 
 def chunked(iterator, chunk_size):
+    """
+    Given an iterator, chunk it up into ~chunk_size, but be aware of newline
+    termination as an intended goal.
+    """
     result = ''
     for chunk in iterator:
         result += chunk
         if len(result) > chunk_size:
-            yield result[:chunk_size]
-            result = result[chunk_size:]
+            newline_pos = result.find('\n', chunk_size - 1)
+            if newline_pos == -1:
+                newline_pos = result.rfind('\n')
+                if newline_pos == -1:
+                    newline_pos = chunk_size
+                else:
+                    newline_pos += 1
+            else:
+                newline_pos += 1
+            yield result[:newline_pos]
+            result = result[newline_pos:]
     if result:
         yield result
 
--- tests/changes/backends/jenkins/test_builder.py
+++ tests/changes/backends/jenkins/test_builder.py
@@ -11,7 +11,7 @@ from changes.constants import Status, Result
 from changes.models import (
     Repository, Project, RemoteEntity, TestCase, Patch, LogSource, LogChunk
 )
-from changes.backends.jenkins.builder import JenkinsBuilder
+from changes.backends.jenkins.builder import JenkinsBuilder, chunked
 from changes.testutils import BackendTestCase
 
 
@@ -449,3 +449,25 @@ class SyncBuildTest(BaseTestCase):
 
         entity = RemoteEntity.query.get(entity.id)
         assert entity.data.get('log_offset') == 7
+
+
+class ChunkedTest(TestCase):
+    def test_simple(self):
+        foo = 'aaa\naaa\naaa\n'
+
+        result = list(chunked(foo, 5))
+        assert len(result) == 2
+        assert result[0] == 'aaa\naaa\n'
+        assert result[1] == 'aaa\n'
+
+        result = list(chunked(foo, 8))
+
+        assert len(result) == 1
+        assert result[0] == 'aaa\naaa\naaa\n'
+
+        result = list(chunked(foo, 4))
+
+        assert len(result) == 3
+        assert result[0] == 'aaa\n'
+        assert result[1] == 'aaa\n'
+        assert result[2] == 'aaa\n'
