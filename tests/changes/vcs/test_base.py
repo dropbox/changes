dfa185cbaed909fe44c6e7ac7b7a46355ed47dd8
David Cramer <dcramer@gmail.com>
2013-11-27 12:18:24 -0800

    RevisionResult test

---
+++ tests/changes/vcs/test_base.py
@@ -0,0 +1,31 @@
+from datetime import datetime
+
+from changes.models import Revision
+from changes.vcs.base import RevisionResult
+from changes.testutils.cases import TestCase
+
+
+class RevisionResultTestCase(TestCase):
+    def test_simple(self):
+        repo = self.create_repo()
+        result = RevisionResult(
+            author='Foo Bar <foo@example.com>',
+            committer='Biz Baz <baz@example.com>',
+            author_date=datetime(2013, 9, 19, 22, 15, 22),
+            committer_date=datetime(2013, 9, 19, 22, 15, 23),
+            message='Hello world!',
+            parents=['a' * 40, 'b' * 40],
+
+        )
+        revision = result.save(repo)
+
+        assert type(revision) == Revision
+        assert revision.repository_id == repo.id
+        assert revision.message == 'Hello world!'
+        assert revision.author.name == 'Foo Bar'
+        assert revision.author.email == 'foo@example.com'
+        assert revision.committer.name == 'Biz Baz'
+        assert revision.committer.email == 'baz@example.com'
+        assert revision.parents == ['a' * 40, 'b' * 40]
+        assert revision.author_date == datetime(2013, 9, 19, 22, 15, 22)
+        assert revision.committer_date == datetime(2013, 9, 19, 22, 15, 23)
