f5723e7de82cfbd427f4bb7b138fcadaa58901ca
David Cramer <dcramer@gmail.com>
2013-11-27 12:30:14 -0800

    Correct test

--- tests/changes/vcs/test_base.py
+++ tests/changes/vcs/test_base.py
@@ -9,6 +9,7 @@ class RevisionResultTestCase(TestCase):
     def test_simple(self):
         repo = self.create_repo()
         result = RevisionResult(
+            id='c' * 40,
             author='Foo Bar <foo@example.com>',
             committer='Biz Baz <baz@example.com>',
             author_date=datetime(2013, 9, 19, 22, 15, 22),
@@ -17,15 +18,18 @@ class RevisionResultTestCase(TestCase):
             parents=['a' * 40, 'b' * 40],
 
         )
-        revision = result.save(repo)
+        revision, created = result.save(repo)
+
+        assert created
 
         assert type(revision) == Revision
         assert revision.repository_id == repo.id
+        assert revision.sha == 'c' * 40
         assert revision.message == 'Hello world!'
         assert revision.author.name == 'Foo Bar'
         assert revision.author.email == 'foo@example.com'
         assert revision.committer.name == 'Biz Baz'
         assert revision.committer.email == 'baz@example.com'
         assert revision.parents == ['a' * 40, 'b' * 40]
-        assert revision.author_date == datetime(2013, 9, 19, 22, 15, 22)
-        assert revision.committer_date == datetime(2013, 9, 19, 22, 15, 23)
+        assert revision.date_created == datetime(2013, 9, 19, 22, 15, 22)
+        assert revision.date_committed == datetime(2013, 9, 19, 22, 15, 23)
