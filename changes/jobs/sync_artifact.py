4780ef1600e7019780562e586a6c94a73a001e26
David Cramer <dcramer@gmail.com>
2013-12-03 15:26:51 -0800

    Missing file from 920a5bb

---
+++ changes/jobs/sync_artifact.py
@@ -0,0 +1,33 @@
+from flask import current_app
+
+from changes.backends.jenkins.builder import JenkinsBuilder
+from changes.config import queue
+from changes.models import Build
+
+
+def sync_with_builder(build, artifact):
+    # HACK(dcramer): this definitely is a temporary fix for our "things are
+    # only a single builder" problem
+    builder = JenkinsBuilder(
+        app=current_app,
+        base_url=current_app.config['JENKINS_URL'],
+    )
+    builder.sync_artifact(build, artifact)
+
+
+def sync_artifact(build_id, artifact):
+    try:
+        build = Build.query.get(build_id)
+        if not build:
+            return
+
+        sync_with_builder(build)
+
+    except Exception as exc:
+        # Ensure we continue to synchronize this build as this could be a
+        # temporary failure
+        current_app.logger.exception(
+            'Failed to sync artifact %r, %r', build_id, artifact)
+        raise queue.retry('sync_artifact', kwargs={
+            'build_id': build_id,
+        }, exc=exc, countdown=60)
