46d71626437a8e0f50b9a562b1ee98e26a934a9c
David Cramer <dcramer@gmail.com>
2013-12-05 17:48:41 -0800

    Immediately mark last update attempt when checK-repos fires

--- changes/jobs/check_repos.py
+++ changes/jobs/check_repos.py
@@ -1,6 +1,7 @@
 from datetime import datetime, timedelta
 from sqlalchemy import or_
 
+from changes.config import db
 from changes.config import queue
 from changes.models import Repository, RepositoryBackend
 
@@ -13,13 +14,22 @@ def check_repos():
     now = datetime.utcnow()
     cutoff = now - timedelta(minutes=3)
 
-    repo_list = Repository.query.filter(
+    repo_list = list(Repository.query.filter(
         Repository.backend != RepositoryBackend.unknown,
         or_(
             Repository.last_update_attempt < cutoff,
             Repository.last_update_attempt == None,  # NOQA
         )
-    )
+    ))
+    if not repo_list:
+        return
+
+    db.session.query(Repository).filter(
+        Repository.id.in_([r.id for r in repo_list]),
+    ).update({
+        Repository.last_update_attempt: now,
+    }, synchronize_session=False)
+
     for repo in repo_list:
         queue.delay('sync_repo', kwargs={
             'repo_id': repo.id.hex,
