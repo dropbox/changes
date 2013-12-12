13175f1d0a229a7886790048cee5c641e43743ce
David Cramer <dcramer@gmail.com>
2013-12-11 16:28:08 -0800

    leave repo resync up to check_repos

--- changes/jobs/sync_repo.py
+++ changes/jobs/sync_repo.py
@@ -1,5 +1,4 @@
 from datetime import datetime
-from flask import current_app
 
 from changes.config import db, queue
 from changes.models import Repository
@@ -18,39 +17,31 @@ def sync_repo(repo_id):
     db.session.add(repo)
     db.session.commit()
 
-    try:
-        if vcs.exists():
-            vcs.update()
-        else:
-            vcs.clone()
+    if vcs.exists():
+        vcs.update()
+    else:
+        vcs.clone()
 
-        # TODO(dcramer): this doesnt scrape everything, and really we wouldn't
-        # want to do this all in a single job so we should split this into a
-        # backfill task
-        might_have_more = True
-        parent = None
-        while might_have_more:
-            might_have_more = False
-            for commit in vcs.log(parent=parent):
-                revision, created = commit.save(repo)
-                db.session.commit()
-                if not created:
-                    break
-                might_have_more = True
-                parent = commit.id
+    # TODO(dcramer): this doesnt scrape everything, and really we wouldn't
+    # want to do this all in a single job so we should split this into a
+    # backfill task
+    might_have_more = True
+    parent = None
+    while might_have_more:
+        might_have_more = False
+        for commit in vcs.log(parent=parent):
+            revision, created = commit.save(repo)
+            db.session.commit()
+            if not created:
+                break
+            might_have_more = True
+            parent = commit.id
 
-        repo.last_update = datetime.utcnow()
+    repo.last_update = datetime.utcnow()
 
-        db.session.add(repo)
-        db.session.commit()
+    db.session.add(repo)
+    db.session.commit()
 
-        queue.delay('sync_repo', kwargs={
-            'repo_id': repo_id
-        }, countdown=15)
-
-    except Exception as exc:
-        # should we actually use retry support here?
-        current_app.logger.exception('Failed to sync repository %s', repo_id)
-        raise queue.retry('sync_repo', kwargs={
-            'repo_id': repo_id,
-        }, exc=exc, countdown=120)
+    queue.delay('sync_repo', kwargs={
+        'repo_id': repo_id
+    }, countdown=15)
