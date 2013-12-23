1d463462bd2c198f028c526318643f9af003a7ec
David Cramer <cramer@dropbox.com>
2013-12-23 14:20:08 -0800

    lint

--- changes/jobs/sync_build.py
+++ changes/jobs/sync_build.py
@@ -8,7 +8,7 @@ from changes.backends.base import UnrecoverableException
 from changes.backends.jenkins.builder import JenkinsBuilder
 from changes.config import db, queue
 from changes.constants import Status, Result
-from changes.models import Build, BuildPlan, Plan, Project, RemoteEntity
+from changes.models import Build, BuildPlan, Plan, RemoteEntity
 from changes.utils.locking import lock
 
 
