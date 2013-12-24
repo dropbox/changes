a35876cfd83067dde9b0071cd368514f3d206ad5
David Cramer <cramer@dropbox.com>
2013-12-23 22:23:09 -0800

    Correctly handle mutability on JSONEncodedDict

--- changes/db/types/json.py
+++ changes/db/types/json.py
@@ -1,8 +1,36 @@
 from __future__ import absolute_import
 
+import json
+
+from sqlalchemy.ext.mutable import Mutable
 from sqlalchemy.types import TypeDecorator, Unicode
 
-import json
+
+class MutableDict(Mutable, dict):
+    @classmethod
+    def coerce(cls, key, value):
+        "Convert plain dictionaries to MutableDict."
+
+        if not isinstance(value, MutableDict):
+            if isinstance(value, dict):
+                return MutableDict(value)
+
+            # this call will raise ValueError
+            return Mutable.coerce(key, value)
+        else:
+            return value
+
+    def __setitem__(self, key, value):
+        "Detect dictionary set events and emit change events."
+
+        dict.__setitem__(self, key, value)
+        self.changed()
+
+    def __delitem__(self, key):
+        "Detect dictionary del events and emit change events."
+
+        dict.__delitem__(self, key)
+        self.changed()
 
 
 class JSONEncodedDict(TypeDecorator):
@@ -19,3 +47,5 @@ class JSONEncodedDict(TypeDecorator):
             return json.loads(value)
 
         return {}
+
+MutableDict.associate_with(JSONEncodedDict)
