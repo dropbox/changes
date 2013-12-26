fd620a43ecf0897083f13dac3ace5cd5d842508e
David Cramer <cramer@dropbox.com>
2013-12-26 15:58:18 -0800

    Add build serializer

---
+++ changes/api/serializer/models/build.py
@@ -0,0 +1,52 @@
+from changes.api.serializer import Serializer, register
+from changes.models import Build
+
+
+@register(Build)
+class BuildSerializer(Serializer):
+    def serialize(self, instance):
+        if instance.project_id:
+            avg_build_time = instance.project.avg_build_time
+        else:
+            avg_build_time = None
+
+        target = instance.target
+        if target is None and instance.revision_sha:
+            target = instance.revision_sha[:12]
+
+        if instance.revision_sha:
+            revision = {
+                'sha': instance.revision_sha,
+            }
+        else:
+            revision = None
+
+        if instance.patch_id:
+            patch = {
+                'id': instance.patch_id.hex,
+            }
+        else:
+            patch = None
+
+        return {
+            'id': instance.id.hex,
+            'name': instance.label,
+            'target': target,
+            'result': instance.result,
+            'status': instance.status,
+            'project': instance.project,
+            'cause': instance.cause,
+            'author': instance.author,
+            'source': {
+                'revision': revision,
+                'patch': patch,
+            },
+            'message': instance.message,
+            'duration': instance.duration,
+            'estimatedDuration': avg_build_time,
+            'link': '/builds/%s/' % (instance.id.hex,),
+            'dateCreated': instance.date_created.isoformat(),
+            'dateModified': instance.date_modified.isoformat() if instance.date_modified else None,
+            'dateStarted': instance.date_started.isoformat() if instance.date_started else None,
+            'dateFinished': instance.date_finished.isoformat() if instance.date_finished else None,
+        }
---
+++ tests/changes/api/serializer/models/test_build.py
@@ -0,0 +1,35 @@
+from datetime import datetime
+from uuid import UUID
+
+from changes.api.serializer import serialize
+from changes.models import Build, Project
+
+
+def test_simple():
+    build = Build(
+        id=UUID(hex='33846695b2774b29a71795a009e8168a'),
+        label='Hello world',
+        target='D1234',
+        message='Foo bar',
+        project=Project(slug='test', name='test'),
+        revision_sha='1e7958a368f44b0eb5a57372a9910d50',
+        date_created=datetime(2013, 9, 19, 22, 15, 22),
+        date_started=datetime(2013, 9, 19, 22, 15, 23),
+        date_finished=datetime(2013, 9, 19, 22, 15, 33),
+    )
+    result = serialize(build)
+    assert result['name'] == 'Hello world'
+    assert result['link'] == '/builds/33846695b2774b29a71795a009e8168a/'
+    assert result['id'] == '33846695b2774b29a71795a009e8168a'
+    assert result['source'] == {
+        'revision': {
+            'sha': '1e7958a368f44b0eb5a57372a9910d50',
+        },
+        'patch': None,
+    }
+    assert result['target'] == 'D1234'
+    assert result['message'] == 'Foo bar'
+    assert result['dateCreated'] == '2013-09-19T22:15:22'
+    assert result['dateStarted'] == '2013-09-19T22:15:23'
+    assert result['dateFinished'] == '2013-09-19T22:15:33'
+    assert result['duration'] == 10000
