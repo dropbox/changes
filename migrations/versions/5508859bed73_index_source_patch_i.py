ec4f9bcce02d8d7a8b0ba5286f55fbc97589514b
David Cramer <cramer@dropbox.com>
2014-01-06 11:16:57 -0800

    Correct base revision

--- migrations/versions/5508859bed73_index_source_patch_i.py
+++ migrations/versions/5508859bed73_index_source_patch_i.py
@@ -1,14 +1,14 @@
 """Index Source.patch_id
 
 Revision ID: 5508859bed73
-Revises: 3042d0ca43bf
+Revises: 26f665189ca0
 Create Date: 2014-01-06 11:14:19.109932
 
 """
 
 # revision identifiers, used by Alembic.
 revision = '5508859bed73'
-down_revision = '3042d0ca43bf'
+down_revision = '26f665189ca0'
 
 from alembic import op
 
