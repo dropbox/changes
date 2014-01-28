64ebd42a54f92182051606171bc6b0be9d784970
David Cramer <cramer@dropbox.com>
2014-01-27 17:23:15 -0800

    Dont treat 0 as empty value

--- static/js/directives/duration.js
+++ static/js/directives/duration.js
@@ -6,7 +6,7 @@
       return function durationDirective(scope, element, attrs) {
         var $element = $(element);
         scope.$watch(attrs.duration, function(value) {
-          if (!value) {
+          if (value === null) {
             return 'n/a';
           }
           element.text(duration(value));
