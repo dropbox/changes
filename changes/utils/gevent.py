2200a8a94879b39a6ba5ee1680e665537016d0e2
David Cramer <cramer@dropbox.com>
2014-02-05 16:07:46 -0800

    Ensure gevent is patched immediately

--- changes/utils/gevent.py
+++
@@ -1,9 +0,0 @@
-from __future__ import absolute_import
-
-
-def patch_gevent():
-    from gevent import monkey
-    monkey.patch_all()
-
-    from changes.db import psyco_gevent
-    psyco_gevent.make_psycopg_green()
