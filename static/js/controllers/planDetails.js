acf466760fb7f2c21d47add147f7220e10b1e16f
David Cramer <cramer@dropbox.com>
2014-02-20 14:07:22 -0800

    Lint

--- static/js/controllers/planDetails.js
+++ static/js/controllers/planDetails.js
@@ -2,7 +2,7 @@ define(['app'], function(app) {
   'use strict';
 
   app.controller('planDetailsCtrl', ['initial', '$scope', 'collection', function(initial, $scope, Collection) {
-    $scope.plan = initial.data
+    $scope.plan = initial.data;
     $scope.projectList = new Collection($scope, initial.data.projects);
     $scope.stepList = new Collection($scope, initial.data.steps);
   }]);
