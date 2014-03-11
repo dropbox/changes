6fa0f83d9b58e33906dda007832b2ae036fab264
David Cramer <cramer@dropbox.com>
2014-03-11 11:45:43 -0700

    Use log provider

--- static/js/modules/stream.js
+++ static/js/modules/stream.js
@@ -2,41 +2,41 @@ define(['angular'], function(angular) {
   'use strict';
 
   angular.module('stream', [])
-    .factory('Stream', ['$window', function(window){
-      if (window.streams === undefined) {
-        window.streams = {};
+    .factory('Stream', function($window, $log){
+      if ($window.streams === undefined) {
+        $window.streams = {};
       }
 
       return function($scope, url, callback) {
-        if (window.streams[url]) {
-          console.log('[Stream] Closing connection to ' + url);
+        if ($window.streams[url]) {
+          $log.info('[Stream] Closing connection to ' + url);
           window.streams[url].close();
         }
-        console.log('[Stream] Initiating connection to ' + url);
+        $log.info('[Stream] Initiating connection to ' + url);
 
-        window.streams[url] = new EventSource(url + '?_=' + new Date().getTime());
-        window.streams[url].onopen = function(e) {
-          console.log('[Stream] Connection opened to ' + url);
+        $window.streams[url] = new EventSource(url + '?_=' + new Date().getTime());
+        $window.streams[url].onopen = function(e) {
+          $log.info('[Stream] Connection opened to ' + url);
         };
 
         $scope.$on("$destroy", function() {
-          if (!window.streams[url]) {
+          if (!$window.streams[url]) {
             return;
           }
-          console.log('[Stream] Closing connection to ' + url);
-          window.streams[url].close();
+          $log.info('[Stream] Closing connection to ' + url);
+          $window.streams[url].close();
           delete window.streams[url];
         });
 
         return {
           subscribe: function(event, callback){
-            window.streams[url].addEventListener(event, function(e) {
+            $window.streams[url].addEventListener(event, function(e) {
               var data = $.parseJSON(e.data);
-              console.log('[Stream] Got event for ' + event);
+              $log.info('[Stream] Got event for ' + event);
               callback(data);
             }, false);
           }
         };
       };
-    }]);
+    });
 });
