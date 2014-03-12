f668b393a412608d1b932447338077592b4d6b3c
David Cramer <cramer@dropbox.com>
2014-03-12 15:10:15 -0700

    Restrict nodes to job runs within last week

--- changes/api/node_index.py
+++ changes/api/node_index.py
@@ -1,11 +1,21 @@
 from __future__ import absolute_import
 
+from datetime import datetime, timedelta
+
 from changes.api.base import APIView
-from changes.models import Node
+from changes.models import Node, JobStep
 
 
 class NodeIndexAPIView(APIView):
     def get(self):
-        queryset = Node.query.order_by(Node.label.asc())
+        cutoff = datetime.utcnow() - timedelta(days=7)
+
+        queryset = Node.query.join(
+            JobStep, JobStep.node_id == Node.id,
+        ).filter(
+            JobStep.date_created > cutoff,
+        ).order_by(Node.label.asc()).group_by(Node)
+
+        print queryset
 
         return self.paginate(queryset)
