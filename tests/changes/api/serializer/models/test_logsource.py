2a40fb30f679347d72491f721dca121f96b57acd
David Cramer <cramer@dropbox.com>
2014-03-20 15:50:10 -0700

    Update test to match new behavior

--- tests/changes/api/serializer/models/test_logsource.py
+++ tests/changes/api/serializer/models/test_logsource.py
@@ -10,7 +10,10 @@ def test_simple():
         id=UUID(hex='33846695b2774b29a71795a009e8168a'),
         job_id=UUID(hex='2e18a7cbc0c24316b2ef9d41fea191d6'),
         job=Job(id=UUID(hex='2e18a7cbc0c24316b2ef9d41fea191d6')),
-        step=JobStep(id=UUID(hex='36c7af5e56aa4a7fbf076e13ac00a866')),
+        step=JobStep(
+            id=UUID(hex='36c7af5e56aa4a7fbf076e13ac00a866'),
+            phase_id=UUID(hex='46c7af5e56aa4a7fbf076e13ac00a866')
+        ),
         name='console',
         date_created=datetime(2013, 9, 19, 22, 15, 22),
     )
