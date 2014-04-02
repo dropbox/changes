630d34676b053294845526a883dcd2f51bf6d133
David Cramer <cramer@dropbox.com>
2014-04-01 17:01:26 -0700

    Simplify migrations

--- migrations/env.py
+++ migrations/env.py
@@ -37,35 +37,8 @@ target_metadata = db.metadata
 # force registration of models
 import changes.models  # NOQA
 
-# other values from the config, defined by the needs of env.py,
-# can be acquired:
-# my_important_option = config.get_main_option("my_important_option")
-# ... etc.
 
-
-def run_migrations_offline():
-    """Run migrations in 'offline' mode.
-
-    This configures the context with just a URL
-    and not an Engine, though an Engine is acceptable
-    here as well.  By skipping the Engine creation
-    we don't even need a DBAPI to be available.
-
-    Calls to context.execute() here emit the given string to the
-    script output.
-
-    """
-    connection = db.engine.connect()
-    context.configure(
-        connection=connection,
-        target_metadata=target_metadata,
-    )
-
-    with context.begin_transaction():
-        context.run_migrations()
-
-
-def run_migrations_online():
+def run_migrations():
     """Run migrations in 'online' mode.
 
     In this scenario we need to create an Engine
@@ -84,7 +57,4 @@ def run_migrations_online():
     finally:
         connection.close()
 
-if context.is_offline_mode():
-    run_migrations_offline()
-else:
-    run_migrations_online()
+run_migrations()
