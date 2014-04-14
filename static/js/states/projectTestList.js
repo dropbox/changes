60d024e18c2da1d64a40c15e929e9695b73e2430
David Cramer <cramer@dropbox.com>
2014-04-14 11:04:59 -0700

    Add pagination to project test list (UI)

--- static/js/states/projectTestList.js
+++ static/js/states/projectTestList.js
@@ -1,4 +1,7 @@
-define(['app'], function(app) {
+define([
+  'app',
+  'utils/parseLinkHeader'
+], function(app, parseLinkHeader) {
   'use strict';
 
   return {
@@ -6,7 +9,34 @@ define(['app'], function(app) {
     url: 'tests/',
     templateUrl: 'partials/project-test-list.html',
     controller: function($scope, testList) {
-      $scope.tests = testList.data;
+      function loadTestList(url) {
+        if (!url) {
+          return;
+        }
+        $http.get(url)
+          .success(function(data, status, headers){
+            $scope.testList = data;
+            $scope.pageLinks = parseLinkHeader(headers('Link'));
+          });
+      }
+
+      $scope.loadPreviousPage = function() {
+        $(document.body).scrollTop(0);
+        loadTestList($scope.pageLinks.previous);
+      };
+
+      $scope.loadNextPage = function() {
+        $(document.body).scrollTop(0);
+        loadTestList($scope.pageLinks.next);
+      };
+
+      $scope.$watch("pageLinks", function(value) {
+        $scope.nextPage = value.next || null;
+        $scope.previousPage = value.previous || null;
+      });
+
+      $scope.pageLinks = parseLinkHeader(testList.headers('Link'));
+      $scope.testList = testList.data;
     },
     resolve: {
       testList: function($http, projectData) {
