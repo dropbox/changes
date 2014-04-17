120784bb17276b15b13b940f501d8c236c208183
David Cramer <cramer@dropbox.com>
2014-04-16 17:03:16 -0700

    Prevent recursion with @eager_tasks by disabling retries

--- changes/testutils/helpers.py
+++ changes/testutils/helpers.py
@@ -1,13 +1,18 @@
-from changes.config import queue
 from functools import wraps
+from mock import patch
+
+from changes.config import queue
+from changes.queue.task import TooManyRetries
 
 
 def eager_tasks(func):
     @wraps(func)
+    # prevent retries due to recursion issues
     def wrapped(*args, **kwargs):
-        queue.celery.conf.CELERY_ALWAYS_EAGER = True
-        try:
-            return func(*args, **kwargs)
-        finally:
-            queue.celery.conf.CELERY_ALWAYS_EAGER = False
+        with patch('changes.queue.task.TrackedTask._retry', side_effect=TooManyRetries()):
+            queue.celery.conf.CELERY_ALWAYS_EAGER = True
+            try:
+                return func(*args, **kwargs)
+            finally:
+                queue.celery.conf.CELERY_ALWAYS_EAGER = False
     return wrapped
