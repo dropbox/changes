dd1ae72c92c94c8686b22404fa449fc2f6e65708
David Cramer <cramer@dropbox.com>
2014-04-17 16:44:00 -0700

    debugtoolbar version 0.9.*

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ install_requires = [
     'enum34>=0.9.18,<0.10.0',
     'flask>=0.10.1,<0.11.0',
     'flask-actions>=0.6.6,<0.7.0',
-    'flask-debugtoolbar>=0.8.0,<=0.10.0',
+    'flask-debugtoolbar>=0.9.0,<0.10.0',
     'flask-mail>=0.9.0,<0.10.0',
     'flask-restful>=0.2.10,<0.2.11',
     'flask-sqlalchemy>=1.0,<1.1',
