1d97a343e536159b5d1d572c76996cd6ae975255
David Cramer <cramer@dropbox.com>
2014-04-22 16:40:50 -0700

    revert 365e4abca73d55fe4ba1b51a0057556ff8487c41

--- changes/listeners/build_revision.py
+++ changes/listeners/build_revision.py
@@ -5,7 +5,7 @@ from fnmatch import fnmatch
 
 from changes.api.build_index import BuildIndexAPIView
 from changes.config import db
-from changes.models import ItemOption
+from changes.models import ItemOption, Project
 
 
 logger = logging.getLogger('build_revision')
@@ -22,27 +22,35 @@ def should_build_branch(revision, allowed_branches):
 
 
 def revision_created_handler(revision, **kwargs):
+    project_list = list(Project.query.filter(
+        Project.repository_id == revision.repository_id,
+    ))
+    if not project_list:
+        return
+
     options = dict(
         db.session.query(
-            ItemOption.name, ItemOption.value
+            ItemOption.item_id, ItemOption.value
         ).filter(
-            ItemOption.item_id == revision.repository_id,
+            ItemOption.item_id.in_(p.id for p in project_list),
             ItemOption.name.in_([
                 'build.branch-names',
             ])
         )
     )
 
-    if not should_build_branch(revision, options.get('build.branch-names', '*').split(' ')):
-        return
+    for project in project_list:
+        branch_names = options.get('build.branch-names', '*').split(' ')
+        if not should_build_branch(revision, branch_names):
+            return
 
-    data = {
-        'sha': revision.sha,
-        'repository': revision.repository.url,
-    }
-    with current_app.test_request_context('/api/0/builds/', method='POST', data=data):
-        response = BuildIndexAPIView().post()
-        if isinstance(response, (list, tuple)):
-            response, status = response
-            if status != 200:
-                logger.error('Failed to create builds: %s' % (response,))
+        data = {
+            'sha': revision.sha,
+            'project': project.slug,
+        }
+        with current_app.test_request_context('/api/0/builds/', method='POST', data=data):
+            response = BuildIndexAPIView().post()
+            if isinstance(response, (list, tuple)):
+                response, status = response
+                if status != 200:
+                    logger.error('Failed to create builds: %s' % (response,))
