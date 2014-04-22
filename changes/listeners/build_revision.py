365e4abca73d55fe4ba1b51a0057556ff8487c41
David Cramer <cramer@dropbox.com>
2014-04-22 16:39:18 -0700

    Revert "Move build.branch-names to project settings"
    This reverts commit a38fc17616ae160aa41046470964034294eade1a.

--- changes/listeners/build_revision.py
+++ changes/listeners/build_revision.py
@@ -5,7 +5,7 @@ from fnmatch import fnmatch
 
 from changes.api.build_index import BuildIndexAPIView
 from changes.config import db
-from changes.models import ItemOption, Project
+from changes.models import ItemOption
 
 
 logger = logging.getLogger('build_revision')
@@ -22,35 +22,27 @@ def should_build_branch(revision, allowed_branches):
 
 
 def revision_created_handler(revision, **kwargs):
-    project_list = list(Project.query.filter(
-        Project.repository_id == revision.repository_id,
-    ))
-    if not project_list:
-        return
-
     options = dict(
         db.session.query(
-            ItemOption.item_id, ItemOption.value
+            ItemOption.name, ItemOption.value
         ).filter(
-            ItemOption.item_id.in_(p.id for p in project_list),
+            ItemOption.item_id == revision.repository_id,
             ItemOption.name.in_([
                 'build.branch-names',
             ])
         )
     )
 
-    for project in project_list:
-        branch_names = options.get('build.branch-names', '*').split(' ')
-        if not should_build_branch(revision, branch_names):
-            return
+    if not should_build_branch(revision, options.get('build.branch-names', '*').split(' ')):
+        return
 
-        data = {
-            'sha': revision.sha,
-            'project': project.slug,
-        }
-        with current_app.test_request_context('/api/0/builds/', method='POST', data=data):
-            response = BuildIndexAPIView().post()
-            if isinstance(response, (list, tuple)):
-                response, status = response
-                if status != 200:
-                    logger.error('Failed to create builds: %s' % (response,))
+    data = {
+        'sha': revision.sha,
+        'repository': revision.repository.url,
+    }
+    with current_app.test_request_context('/api/0/builds/', method='POST', data=data):
+        response = BuildIndexAPIView().post()
+        if isinstance(response, (list, tuple)):
+            response, status = response
+            if status != 200:
+                logger.error('Failed to create builds: %s' % (response,))
