4d40e9db4bd6b58787557e8d5547f69eb67c9b96
David Cramer <cramer@dropbox.com>
2014-04-24 16:48:48 -0700

    Add additional coverage to author build list

--- tests/changes/api/test_author_build_index.py
+++ tests/changes/api/test_author_build_index.py
@@ -18,11 +18,21 @@ class AuthorBuildListTest(APITestCase):
         data = self.unserialize(resp)
         assert len(data) == 0
 
-        author = Author(email='foo@example.com', name='Foo Bar')
+        author = Author(email=self.default_user.email, name='Foo Bar')
         db.session.add(author)
         build = self.create_build(self.project, author=author)
 
         path = '/api/0/authors/{0}/builds/'.format(author.id.hex)
+
+        resp = self.client.get(path)
+        assert resp.status_code == 200
+        data = self.unserialize(resp)
+        assert len(data) == 1
+        assert data[0]['id'] == build.id.hex
+
+        self.login(self.default_user)
+
+        path = '/api/0/authors/me/builds/'
 
         resp = self.client.get(path)
         assert resp.status_code == 200
