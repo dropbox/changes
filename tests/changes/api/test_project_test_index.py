bb89e9685cbff6cf875e5fb9f7798aad5968d442
David Cramer <cramer@dropbox.com>
2014-04-24 11:37:21 -0700

    Add min_duration query parameter to project test index

--- tests/changes/api/test_project_test_index.py
+++ tests/changes/api/test_project_test_index.py
@@ -15,7 +15,7 @@ class ProjectTestIndexTest(APITestCase):
         build = self.create_build(project)
         job = self.create_job(
             build, status=Status.finished, result=Result.passed)
-        test = self.create_test(job=job, name='foobar')
+        test = self.create_test(job=job, name='foobar', duration=50)
 
         path = '/api/0/projects/{0}/tests/'.format(fake_project_id.hex)
 
@@ -50,6 +50,23 @@ class ProjectTestIndexTest(APITestCase):
         assert data[0]['project']['id'] == project.id.hex
 
         path = '/api/0/projects/{0}/tests/?query=hello'.format(project.id.hex)
+
+        resp = self.client.get(path)
+        assert resp.status_code == 200
+        data = self.unserialize(resp)
+        assert len(data) == 0
+
+        # test duration
+        path = '/api/0/projects/{0}/tests/?min_duration=50'.format(project.id.hex)
+
+        resp = self.client.get(path)
+        assert resp.status_code == 200
+        data = self.unserialize(resp)
+        assert len(data) == 1
+        assert data[0]['hash'] == test.name_sha
+        assert data[0]['project']['id'] == project.id.hex
+
+        path = '/api/0/projects/{0}/tests/?min_duration=51'.format(project.id.hex)
 
         resp = self.client.get(path)
         assert resp.status_code == 200
