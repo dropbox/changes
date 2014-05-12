8a1c78d24c85290553ba9a3d67e1cca8b18238df
David Cramer <cramer@dropbox.com>
2014-05-12 10:29:57 -0700

    Add test collector build step for Jenkins
    Reviewers: cf
    
    Reviewed By: cf
    
    Subscribers: haoyi, cf, zviad, ziga
    
    Differential Revision: https://tails.corp.dropbox.com/D46284

---
+++ changes/backends/jenkins/buildsteps/test_collector.py
@@ -0,0 +1,166 @@
+from __future__ import absolute_import
+
+from collections import defaultdict
+from hashlib import md5
+from operator import itemgetter
+
+from changes.api.client import api_client
+from changes.backends.jenkins.buildsteps.collector import JenkinsCollectorBuildStep
+from changes.config import db
+from changes.constants import Status
+from changes.db.utils import get_or_create
+from changes.jobs.sync_job_step import sync_job_step
+from changes.models import JobPhase, JobStep
+
+
+class JenkinsTestCollectorBuildStep(JenkinsCollectorBuildStep):
+    """
+    Fires off a generic job with parameters:
+
+        CHANGES_BID = UUID
+        CHANGES_PID = project slug
+        REPO_URL    = repository URL
+        REPO_VCS    = hg/git
+        REVISION    = sha/id of revision
+        PATCH_URL   = patch to apply, if available
+        SCRIPT      = command to run
+
+    A "tests.json" is expected to be collected as an artifact with the following
+    values:
+
+        {
+            "phase": "optional phase name",
+            "cmd": "py.test --junit=junit.xml {test_names}"
+            "tests": [
+                "foo.bar.test_baz",
+                "foo.bar.test_bar"
+            ]
+        }
+
+    The collected tests will be sorted and partitioned evenly across a set number
+    of shards with the <cmd> value being passed a space-delimited list of tests.
+    """
+    # TODO(dcramer): longer term we'd rather have this create a new phase which
+    # actually executes a different BuildStep (e.g. of order + 1), but at the
+    # time of writing the system only supports a single build step.
+    def __init__(self, job_name=None, script=None, cluster=None, max_shards=10):
+        self.job_name = job_name
+        self.script = script
+        self.cluster = cluster
+        self.max_shards = max_shards
+
+    def fetch_artifact(self, step, artifact):
+        if artifact['fileName'] == 'tests.json':
+            self._expand_jobs(step, artifact)
+        else:
+            builder = self.get_builder()
+            builder.sync_artifact(step, artifact)
+
+    def get_test_stats(self, project):
+        response = api_client.get('/projects/{project}/'.format(
+            project=project.slug))
+        last_build = response['lastPassingBuild']
+
+        if not last_build:
+            return {}, 0
+
+        response = api_client.get('/builds/{build}/tests/?per_page='.format(
+            build=last_build['id']))
+
+        results = defaultdict(int)
+        total_duration = 0
+        test_count = 0
+        for test in response:
+            results[test['name']] += test['duration']
+            results[test['package']] += test['duration']
+            total_duration += test['duration']
+            test_count += 1
+
+        # the build report can contain different test suites so this isnt the
+        # most accurate
+        if total_duration > 0:
+            avg_test_time = int(total_duration / test_count)
+        else:
+            avg_test_time = 0
+
+        return results, avg_test_time
+
+    def _expand_jobs(self, step, artifact):
+        builder = self.get_builder()
+        artifact_data = builder.fetch_artifact(step, artifact)
+        phase_config = artifact_data.json()
+
+        assert phase_config['cmd']
+        assert '{test_names}' in phase_config['cmd']
+        assert phase_config['tests']
+
+        phase, created = get_or_create(JobPhase, where={
+            'job': step.job,
+            'project': step.project,
+            'label': phase_config.get('phase') or 'Test',
+        }, defaults={
+            'status': Status.queued,
+        })
+
+        test_stats, avg_test_time = self.get_test_stats(step.project)
+
+        def get_test_duration(test):
+            return test_stats.get(test, avg_test_time)
+
+        groups = [[] for _ in range(self.max_shards)]
+        weights = [0] * self.max_shards
+        weighted_tests = [(get_test_duration(t), t) for t in phase_config['tests']]
+        for weight, test in sorted(weighted_tests, reverse=True):
+            low_index, _ = min(enumerate(weights), key=itemgetter(1))
+            weights[low_index] += 1 + weight
+            groups[low_index].append(test)
+
+        for test_list in groups:
+            self._expand_job(phase, {
+                'tests': test_list,
+                'cmd': phase_config['cmd'],
+            })
+
+    def _expand_job(self, phase, job_config):
+        assert job_config['tests']
+
+        label = md5(' '.join(job_config['tests'])).hexdigest()
+
+        step, created = get_or_create(JobStep, where={
+            'job': phase.job,
+            'project': phase.project,
+            'phase': phase,
+            'label': label,
+        }, defaults={
+            'data': {
+                'cmd': job_config['cmd'],
+                'tests': job_config['tests'],
+                'job_name': self.job_name,
+                'build_no': None,
+            },
+            'status': Status.queued,
+        })
+
+        # TODO(dcramer): due to no unique constraints this section of code
+        # presents a race condition when run concurrently
+        if not step.data.get('build_no'):
+            builder = self.get_builder()
+            params = builder.get_job_parameters(
+                step.job, script=step.data['cmd'].format(
+                    test_names=step.data['tests'],
+                ))
+
+            job_data = builder.create_job_from_params(
+                job_id=step.id.hex,
+                params=params,
+                job_name=step.data['job_name'],
+            )
+            step.data.update(job_data)
+            db.session.add(step)
+            db.session.commit()
+
+        sync_job_step.delay_if_needed(
+            step_id=step.id.hex,
+            task_id=step.id.hex,
+            parent_task_id=phase.job.id.hex,
+        )
---
+++ tests/changes/backends/jenkins/buildsteps/test_test_collector.py
@@ -0,0 +1,164 @@
+from __future__ import absolute_import
+
+import mock
+import responses
+
+from changes.backends.jenkins.generic_builder import JenkinsGenericBuilder
+from changes.backends.jenkins.buildsteps.test_collector import JenkinsTestCollectorBuildStep
+from changes.constants import Result, Status
+from changes.models import JobPhase, JobStep
+from changes.testutils import TestCase
+
+
+class JenkinsTestCollectorBuildStepTest(TestCase):
+    def get_buildstep(self):
+        return JenkinsTestCollectorBuildStep(
+            job_name='foo-bar',
+            script='exit 0',
+            cluster='default',
+            max_shards=2,
+        )
+
+    def get_mock_builder(self):
+        return mock.Mock(spec=JenkinsGenericBuilder)
+
+    def test_get_builder(self):
+        builder = self.get_buildstep().get_builder()
+        assert builder.job_name == 'foo-bar'
+        assert builder.script == 'exit 0'
+        assert builder.cluster == 'default'
+
+    @mock.patch.object(JenkinsTestCollectorBuildStep, 'get_builder')
+    def test_default_artifact_handling(self, get_builder):
+        builder = self.get_mock_builder()
+        get_builder.return_value = builder
+
+        build = self.create_build(self.project)
+        job = self.create_job(build, data={
+            'job_name': 'server',
+            'build_no': '35',
+        })
+        phase = self.create_jobphase(job)
+        step = self.create_jobstep(phase, data={
+            'item_id': 13,
+            'job_name': 'server',
+        })
+        artifact = {'fileName': 'junit.xml'}
+
+        buildstep = self.get_buildstep()
+        buildstep.fetch_artifact(step, artifact)
+
+        builder.sync_artifact.assert_called_once_with(step, artifact)
+
+    def test_get_test_stats(self):
+        build = self.create_build(
+            project=self.project,
+            status=Status.finished,
+            result=Result.passed,
+        )
+        job = self.create_job(build)
+        self.create_test(job, name='foo.bar.test_baz', duration=50)
+        self.create_test(job, name='foo.bar.test_bar', duration=25)
+
+        buildstep = self.get_buildstep()
+
+        results, avg_time = buildstep.get_test_stats(self.project)
+
+        assert avg_time == 37
+
+        assert results['foo.bar'] == 75
+        assert results['foo.bar.test_baz'] == 50
+        assert results['foo.bar.test_bar'] == 25
+
+    @responses.activate
+    @mock.patch.object(JenkinsTestCollectorBuildStep, 'get_builder')
+    @mock.patch.object(JenkinsTestCollectorBuildStep, 'get_test_stats')
+    def test_job_expansion(self, get_test_stats, get_builder):
+        """
+        Fairly heavy integration test which mocks out a few things but ensures
+        that generic APIs are called correctly and the tests.json is parsed
+        as expected.
+        """
+        builder = self.get_mock_builder()
+        builder.fetch_artifact.return_value.json.return_value = {
+            'phase': 'Test',
+            'cmd': 'py.test --junit=junit.xml {test_names}',
+            'tests': [
+                'foo.bar.test_baz',
+                'foo.bar.test_bar',
+                'foo.bar.test_biz',
+                'foo.bar.test_buz',
+            ],
+        }
+        builder.create_job_from_params.return_value = {
+            'job_name': 'foo-bar',
+            'build_no': 23,
+        }
+
+        get_builder.return_value = builder
+        get_test_stats.return_value = {
+            'foo.bar.test_baz': 50,
+            'foo.bar.test_bar': 15,
+            'foo.bar.test_biz': 10,
+            'foo.bar.test_buz': 200,
+            'foo.bar': 275,
+        }, 68
+
+        build = self.create_build(self.project)
+        job = self.create_job(build, data={
+            'job_name': 'server',
+            'build_no': '35',
+        })
+        phase = self.create_jobphase(job)
+        step = self.create_jobstep(phase, data={
+            'item_id': 13,
+            'job_name': 'server',
+        })
+        artifact = {
+            'fileName': 'tests.json',
+        }
+
+        buildstep = self.get_buildstep()
+        buildstep.fetch_artifact(step, artifact)
+
+        phase2 = JobPhase.query.filter(
+            JobPhase.job_id == job.id,
+            JobPhase.id != phase.id,
+        ).first()
+
+        assert phase2, 'phase wasnt created'
+        assert phase2.label == 'Test'
+        assert phase2.status == Status.queued
+
+        new_steps = sorted(JobStep.query.filter(
+            JobStep.phase_id == phase2.id
+        ), key=lambda x: x.date_created)
+
+        assert len(new_steps) == 2
+        assert new_steps[0].label == '790ed83d37c20fd5178ddb4f20242ef6'
+        assert new_steps[0].data == {
+            'build_no': 23,
+            'job_name': 'foo-bar',
+            'tests': ['foo.bar.test_buz'],
+            'cmd': 'py.test --junit=junit.xml {test_names}',
+        }
+
+        assert new_steps[1].label == '4984ae5173fdb4166e5454d2494a106d'
+        assert new_steps[1].data == {
+            'build_no': 23,
+            'job_name': 'foo-bar',
+            'tests': ['foo.bar.test_baz', 'foo.bar.test_bar', 'foo.bar.test_biz'],
+            'cmd': 'py.test --junit=junit.xml {test_names}',
+        }
+
+        builder.fetch_artifact.assert_called_once_with(step, artifact)
+        builder.create_job_from_params.assert_any_call(
+            job_name='foo-bar',
+            job_id=new_steps[0].id.hex,
+            params=builder.get_job_parameters.return_value,
+        )
+        builder.create_job_from_params.assert_any_call(
+            job_name='foo-bar',
+            job_id=new_steps[1].id.hex,
+            params=builder.get_job_parameters.return_value,
+        )
