2ccfb54f493bf0ffa07db910514a8429a2c51d73
David Cramer <cramer@dropbox.com>
2014-05-14 11:48:55 -0700

    Improve query patterns on node job list

--- changes/api/node_job_index.py
+++ changes/api/node_job_index.py
@@ -3,8 +3,7 @@ from __future__ import absolute_import
 from sqlalchemy.orm import joinedload
 
 from changes.api.base import APIView
-from changes.api.serializer.models.job import JobWithBuildSerializer
-from changes.models import Job, JobStep, Node
+from changes.models import Build, Job, JobStep, Node
 
 
 class NodeJobIndexAPIView(APIView):
@@ -13,14 +12,27 @@ class NodeJobIndexAPIView(APIView):
         if node is None:
             return '', 404
 
-        jobs = Job.query.join(
+        jobs = list(Job.query.join(
             JobStep, JobStep.job_id == Job.id,
-        ).options(
-            joinedload(Job.build, innerjoin=True),
         ).filter(
             JobStep.node_id == node.id,
-        ).order_by(Job.date_created.desc())
+        ).order_by(Job.date_created.desc()))
 
-        return self.paginate(jobs, serializers={
-            Job: JobWithBuildSerializer(),
-        })
+        build_list = list(Build.query.options(
+            joinedload('project'),
+            joinedload('author'),
+            joinedload('source').joinedload('revision'),
+        ).filter(
+            Build.id.in_(j.build_id for j in jobs),
+        ))
+        build_map = dict(
+            (b, d) for b, d in zip(build_list, self.serialize(build_list))
+        )
+
+        context = []
+        for job, data in zip(jobs, self.serialize(jobs)):
+            print job, data
+            data['build'] = build_map[job.build]
+            context.append(data)
+
+        return self.paginate(context, serialize=False)
