87ede8f178636eb014aafc8ecbf69820fd114e87
David Cramer <cramer@dropbox.com>
2014-05-21 15:09:02 -0700

    Handle job creation failures

--- changes/jobs/create_job.py
+++ changes/jobs/create_job.py
@@ -2,13 +2,23 @@ from flask import current_app
 from sqlalchemy.orm import subqueryload_all
 
 from changes.backends.base import UnrecoverableException
+from changes.config import db
 from changes.constants import Status, Result
 from changes.jobs.sync_job import sync_job
 from changes.models import Job, JobPlan, Plan
 from changes.queue.task import tracked_task
 
 
-@tracked_task(max_retries=None)
+def abort_create(task):
+    job = Job.query.get(task.kwargs['job_id'])
+    job.status = Status.finished
+    job.result = Result.aborted
+    db.session.add(job)
+    db.session.commit()
+    current_app.logger.exception('Unrecoverable exception creating job %s', job.id)
+
+
+@tracked_task(on_abort=abort_create, max_retries=10)
 def create_job(job_id):
     job = Job.query.get(job_id)
     if not job:
