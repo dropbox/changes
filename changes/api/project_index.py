7ac8e0e934dc7939bc9b26042ccd2f1047a6eb55
David Cramer <cramer@dropbox.com>
2014-05-27 15:35:54 -0700

    Optimize queries for project index

--- changes/api/project_index.py
+++ changes/api/project_index.py
@@ -1,13 +1,64 @@
 from __future__ import absolute_import, division, unicode_literals
 
 from flask.ext.restful import reqparse
-from sqlalchemy.orm import contains_eager, joinedload
+from sqlalchemy.orm import aliased, contains_eager, joinedload
 
 from changes.api.base import APIView
 from changes.api.auth import requires_auth
 from changes.config import db
 from changes.constants import Result, Status, ProjectStatus
 from changes.models import Project, Repository, Build, Source
+
+
+def get_latest_builds(project_list):
+    build_subquery = Build.query.options(
+        contains_eager('source'),
+    ).join(
+        Source, Build.source_id == Source.id,
+    ).filter(
+        Source.patch_id == None,  # NOQA
+        Build.status == Status.finished,
+    ).order_by(
+        Build.date_created.desc(),
+    ).limit(1).subquery()
+
+    # TODO(dcramer): we're selecting source twice which is a waste of resources
+    results = db.session.query(
+        Project.id,
+        aliased(Build, build_subquery),
+    ).options(
+        joinedload('author'),
+        joinedload('source').joinedload('revision'),
+    ).filter(
+        Project.id == build_subquery.c.project_id,
+    )
+    return dict(results)
+
+
+def get_passing_builds(project_list):
+    build_subquery = Build.query.options(
+        contains_eager('source'),
+    ).join(
+        Source, Build.source_id == Source.id,
+    ).filter(
+        Source.patch_id == None,  # NOQA
+        Build.result == Result.passed,
+        Build.status == Status.finished,
+    ).order_by(
+        Build.date_created.desc(),
+    ).limit(1).subquery()
+
+    # TODO(dcramer): we're selecting source twice which is a waste of resources
+    results = db.session.query(
+        Project.id,
+        aliased(Build, build_subquery),
+    ).options(
+        joinedload('author'),
+        joinedload('source').joinedload('revision'),
+    ).filter(
+        Project.id == build_subquery.c.project_id,
+    )
+    return dict(results)
 
 
 class ProjectIndexAPIView(APIView):
@@ -25,39 +76,32 @@ class ProjectIndexAPIView(APIView):
 
         context = []
 
-        for project in project_list:
-            data = self.serialize(project)
-            # TODO(dcramer): build serializer is O(N) for stats
-            data['lastBuild'] = Build.query.options(
-                joinedload('project', innerjoin=True),
-                joinedload('author'),
-                contains_eager('source'),
-            ).join(
-                Source, Build.source_id == Source.id,
-            ).filter(
-                Source.patch_id == None,  # NOQA
-                Build.project == project,
-                Build.status == Status.finished,
-            ).order_by(
-                Build.date_created.desc(),
-            ).first()
-            if not data['lastBuild'] or data['lastBuild'].result != Result.passed:
-                data['lastPassingBuild'] = Build.query.options(
-                    joinedload('author'),
-                    contains_eager('source')
-                ).join(
-                    Source, Build.source_id == Source.id,
-                ).filter(
-                    Source.patch_id == None,  # NOQA
-                    Build.project == project,
-                    Build.result == Result.passed,
-                    Build.status == Status.finished,
-                ).order_by(
-                    Build.date_created.desc(),
-                ).first()
-            else:
-                data['lastPassingBuild'] = data['lastBuild']
+        latest_build_results = get_latest_builds(project_list)
+        latest_build_map = dict(
+            zip(latest_build_results.keys(),
+                self.serialize(latest_build_results.values()))
+        )
 
+        passing_build_map = {}
+        missing_passing_builds = set()
+        for project_id, build in latest_build_results.iteritems():
+            if build.result == Result.passed:
+                passing_build_map[project_id] = build
+            else:
+                passing_build_map[project_id] = None
+                missing_passing_builds.add(project_id)
+
+        if missing_passing_builds:
+            passing_build_results = get_passing_builds(project_list)
+            passing_build_map.update(dict(
+                zip(passing_build_results.keys(),
+                    self.serialize(passing_build_results.values()))
+            ))
+
+        for project, data in zip(project_list, self.serialize(project_list)):
+            # TODO(dcramer): build serializer is O(N) for stats
+            data['lastBuild'] = latest_build_map.get(project.id)
+            data['lastPassingBuild'] = passing_build_map.get(project.id)
             context.append(data)
 
         return self.paginate(context)
