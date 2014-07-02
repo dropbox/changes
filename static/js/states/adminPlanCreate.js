b87af15d16a3723825843dab7a6bb2cffc41022b
David Cramer <cramer@dropbox.com>
2014-07-02 15:52:47 -0700

    Correct plan details link

--- static/js/states/adminPlanCreate.js
+++ static/js/states/adminPlanCreate.js
@@ -9,7 +9,7 @@ define(['app'], function(app) {
       $scope.createPlan = function() {
         $http.post('/api/0/plans/', $scope.plan)
           .success(function(data){
-            return $state.go('plan_details', {plan_id: data.id});
+            return $state.go('admin_plan_details', {plan_id: data.id});
           }).error(function(data){
             alert('An error ocurred, and we have yet to implement a way to tell you about it.');
           });
