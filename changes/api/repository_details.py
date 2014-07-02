a563e2e9e8ee933471ad49182b7318db74c5eb5b
David Cramer <cramer@dropbox.com>
2014-07-02 13:20:29 -0700

    Add update repository endpoint

--- changes/api/repository_details.py
+++ changes/api/repository_details.py
@@ -1,13 +1,38 @@
 from __future__ import absolute_import, division, unicode_literals
 
+from flask.ext.restful import reqparse
+
+from changes.api.auth import requires_auth
 from changes.api.base import APIView
-from changes.models import Repository
+from changes.config import db
+from changes.models import Repository, RepositoryBackend
 
 
 class RepositoryDetailsAPIView(APIView):
+    parser = reqparse.RequestParser()
+    parser.add_argument('url', type=unicode)
+    parser.add_argument('backend', choices=('git', 'hg', 'unknown'))
+
     def get(self, repository_id):
         repo = Repository.query.get(repository_id)
         if repo is None:
             return '', 404
+
+        return self.respond(repo)
+
+    @requires_auth
+    def post(self, repository_id):
+        repo = Repository.query.get(repository_id)
+        if repo is None:
+            return '', 404
+
+        args = self.parser.parse_args()
+
+        if args.url:
+            repo.url = args.url
+        if args.backend:
+            repo.backend = RepositoryBackend[args.backend]
+
+        db.session.add(repo)
 
         return self.respond(repo)
--- tests/changes/api/test_repository_details.py
+++ tests/changes/api/test_repository_details.py
@@ -1,3 +1,4 @@
+from changes.models import Repository, RepositoryBackend
 from changes.testutils import APITestCase
 
 
@@ -14,3 +15,44 @@ class RepositoryDetailsTest(APITestCase):
         data = self.unserialize(resp)
         assert data['id'] == repo.id.hex
         assert data['url'] == repo.url
+
+
+class UpdateRepositoryTest(APITestCase):
+    def test_simple(self):
+        repo = self.create_repo(
+            url='https://example.com/bar',
+        )
+
+        path = '/api/0/repositories/{0}/'.format(repo.id)
+
+        # ensure endpoint requires authentication
+        resp = self.client.post(path, data={
+            'url': 'https://example.com/foo'
+        })
+        assert resp.status_code == 401
+
+        self.login_default()
+
+        # test only setting url
+        resp = self.client.post(path, data={
+            'url': 'https://example.com/foo'
+        })
+        assert resp.status_code == 200
+
+        data = self.unserialize(resp)
+        assert data['url'] == 'https://example.com/foo'
+
+        repo = Repository.query.get(repo.id)
+        assert repo.url == 'https://example.com/foo'
+
+        # test only setting backend
+        resp = self.client.post(path, data={
+            'backend': 'git'
+        })
+        assert resp.status_code == 200
+        data = self.unserialize(resp)
+        assert data['backend']['id'] == 'git'
+
+        repo = Repository.query.get(repo.id)
+
+        assert repo.backend == RepositoryBackend.git
