353a9df3ed0e8fbd0d515e4335fa0431c7d5d197
David Cramer <cramer@dropbox.com>
2014-07-07 16:54:20 -0700

    Switch node job list to Paginator

--- static/js/states/nodeDetails.js
+++ static/js/states/nodeDetails.js
@@ -1,63 +1,31 @@
 define([
-  'app',
-  'utils/parseLinkHeader'
-], function(app, parseLinkHeader) {
+  'app'
+], function(app) {
   'use strict';
 
   return {
     parent: 'nodes',
     url: ':node_id/',
     templateUrl: 'partials/node-details.html',
-    controller: function($scope, $http, nodeData, jobList, Collection, PageTitle) {
-      function loadJobList(url) {
-        if (!url) {
-          return;
-        }
-        $http.get(url)
-          .success(function(data, status, headers){
-            $scope.jobList = new Collection(data, {
-              limit: 100
-            });
-            $scope.pageLinks = parseLinkHeader(headers('Link'));
-          });
-      }
-
-      $scope.loadPreviousPage = function() {
-        $(document.body).scrollTop(0);
-        loadJobList($scope.pageLinks.previous);
-      };
-
-      $scope.loadNextPage = function() {
-        $(document.body).scrollTop(0);
-        loadJobList($scope.pageLinks.next);
-      };
-
-      $scope.getBuildStatus = function(build) {
-        if (build.status.id == 'finished') {
-          return build.result.name;
-        } else {
-          return build.status.name;
-        }
-      };
-
-      $scope.$watch("pageLinks", function(value) {
-        $scope.nextPage = value.next || null;
-        $scope.previousPage = value.previous || null;
+    controller: function($scope, nodeData, Collection, PageTitle, Paginator) {
+      var collection = new Collection();
+      var paginator = new Paginator('/api/0/nodes/' + nodeData.id + '/jobs/', {
+        collection: collection
       });
 
-      $scope.pageLinks = parseLinkHeader(jobList.headers('Link'));
+      PageTitle.set('Node ' + nodeData.name);
 
-      $scope.node = nodeData.data;
-      $scope.jobList = new Collection(jobList.data);
+      $scope.node = nodeData;
 
-      PageTitle.set('Node ' + $scope.node.name);
+      $scope.jobList = collection;
+      $scope.jobPaginator = paginator;
     },
     resolve: {
       nodeData: function($http, $stateParams) {
-        return $http.get('/api/0/nodes/' + $stateParams.node_id + '/');
-      },
-      jobList: function($http, $stateParams) {
-        return $http.get('/api/0/nodes/' + $stateParams.node_id + '/jobs/');
+        return $http.get('/api/0/nodes/' + $stateParams.node_id + '/')
+          .then(function(response){
+            return response.data;
+          });
       }
     }
   };
