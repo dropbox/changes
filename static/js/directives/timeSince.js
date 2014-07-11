32f9391245ceab9496dcff5875f187bc8cc16b28
David Cramer <cramer@dropbox.com>
2014-07-10 17:11:46 -0700

    Switch from moment.fromNow to utils.time.timeSince

--- static/js/directives/timeSince.js
+++ static/js/directives/timeSince.js
@@ -1,46 +1,35 @@
-(function(){
+define(['app', 'utils'], function(app, utils) {
   'use strict';
 
-  define(['app', 'moment'], function(app, moment) {
-    app.directive('timeSince', ['$timeout', function($timeout) {
-      return function timeSince(scope, element, attrs) {
-        var $element = $(element),
-            timeout_id;
+  app.directive('timeSince', ['$timeout', function($timeout) {
+    return function timeSince(scope, element, attrs) {
+      var $element = $(element),
+          timeout_id;
 
-        function tick(){
-          var value = scope.$eval(attrs.timeSince),
-              time = moment.utc(value),
-              age = moment().diff(time, 'minute'),
-              seconds_until_update;
+      function tick(){
+        var value = scope.$eval(attrs.timeSince),
+            age = Math.abs(value / 1000 / 60),
+            seconds_until_update;
 
-          element.text(time.fromNow());
+        element.text(utils.time.timeSince(value));
 
-          if (age < 1) {
-            seconds_until_update = 1;
-          } else if (age < 60) {
-            seconds_until_update = 30;
-          } else if (age < 180) {
-            seconds_until_update = 300;
-          } else {
-            seconds_until_update = 3600;
-          }
-          timeout_id = $timeout(tick, seconds_until_update * 1000);
+        if (age < 1) {
+          seconds_until_update = 1;
+        } else if (age < 60) {
+          seconds_until_update = 30;
+        } else if (age < 180) {
+          seconds_until_update = 300;
+        } else {
+          seconds_until_update = 3600;
         }
+        timeout_id = $timeout(tick, seconds_until_update * 1000);
+      }
 
-        scope.$watch(attrs.timeSince, function(value){
-          if (!value) {
-            return '';
-          }
+      element.bind('$destroy', function() {
+        $timeout.cancel(timeout_id);
+      });
 
-          element.text(moment.utc(value).fromNow());
-        });
-
-        element.bind('$destroy', function() {
-          $timeout.cancel(timeout_id);
-        });
-
-        tick();
-      };
-    }]);
-  });
-})();
+      tick();
+    };
+  }]);
+});
