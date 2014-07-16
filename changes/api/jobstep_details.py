d2467c3fa46cc58fd6755cdd4b5ce9c44b2d744c
David Cramer <cramer@dropbox.com>
2014-07-16 12:02:18 -0700

    Add basic status/result reporting to jobstep details

--- changes/api/jobstep_details.py
+++ changes/api/jobstep_details.py
@@ -1,12 +1,25 @@
 from __future__ import absolute_import
 
+from datetime import datetime
+from flask_restful.reqparse import RequestParser
 from sqlalchemy.orm import joinedload
 
 from changes.api.base import APIView
+from changes.config import db
+from changes.constants import Result, Status
 from changes.models import JobStep
 
 
+RESULT_CHOICES = ('failed', 'passed')
+
+STATUS_CHOICES = ('queued', 'in_progress', 'finished')
+
+
 class JobStepDetailsAPIView(APIView):
+    post_parser = RequestParser()
+    post_parser.add_argument('status', choices=STATUS_CHOICES)
+    post_parser.add_argument('result', choices=RESULT_CHOICES)
+
     def get(self, step_id):
         jobstep = JobStep.query.options(
             joinedload('project', innerjoin=True),
@@ -14,6 +27,35 @@ class JobStepDetailsAPIView(APIView):
         if jobstep is None:
             return '', 404
 
-        context = self.serialize(jobstep)
+        return self.respond(jobstep, serialize=False)
 
-        return self.respond(context)
+    def post(self, step_id):
+        jobstep = JobStep.query.options(
+            joinedload('project', innerjoin=True),
+        ).get(step_id)
+        if jobstep is None:
+            return '', 404
+
+        args = self.post_parser.parse_args()
+
+        if args.result:
+            jobstep.result = Result[args.result]
+
+        if args.status:
+            jobstep.status = Status[args.status]
+
+            # if we've finished this job, lets ensure we have set date_finished
+            if jobstep.status == Status.finished and jobstep.date_finished is None:
+                jobstep.date_finished = datetime.utcnow()
+            elif jobstep.status != Status.finished and jobstep.date_finished:
+                jobstep.date_finished = None
+
+            if jobstep.status != Status.queued and jobstep.date_started is None:
+                jobstep.date_started = datetime.utcnow()
+            elif jobstep.status == Status.queued and jobstep.date_started:
+                jobstep.date_started = None
+
+        db.session.add(jobstep)
+        db.session.commit()
+
+        return self.respond(jobstep, serialize=False)
--- tests/changes/api/test_jobstep_details.py
+++ tests/changes/api/test_jobstep_details.py
@@ -1,3 +1,5 @@
+from changes.constants import Result, Status
+from changes.models import JobStep
 from changes.testutils import APITestCase
 
 
@@ -15,3 +17,59 @@ class JobStepDetailsTest(APITestCase):
         assert resp.status_code == 200
         data = self.unserialize(resp)
         assert data['id'] == jobstep.id.hex
+
+
+class UpdateJobStepTest(APITestCase):
+    def test_simple(self):
+        project = self.create_project()
+        build = self.create_build(project)
+        job = self.create_job(build)
+        jobphase = self.create_jobphase(job)
+        jobstep = self.create_jobstep(
+            jobphase, status=Status.queued, result=Result.unknown,
+            date_started=None, date_finished=None)
+
+        path = '/api/0/jobsteps/{0}/'.format(jobstep.id.hex)
+
+        resp = self.client.post(path, data={
+            'status': 'in_progress'
+        })
+        assert resp.status_code == 200
+        data = self.unserialize(resp)
+        assert data['id'] == jobstep.id.hex
+
+        jobstep = JobStep.query.get(jobstep.id)
+
+        assert jobstep.status == Status.in_progress
+        assert jobstep.result == Result.unknown
+        assert jobstep.date_started is not None
+        assert jobstep.date_finished is None
+
+        resp = self.client.post(path, data={
+            'status': 'queued'
+        })
+        assert resp.status_code == 200
+        data = self.unserialize(resp)
+        assert data['id'] == jobstep.id.hex
+
+        jobstep = JobStep.query.get(jobstep.id)
+
+        assert jobstep.status == Status.queued
+        assert jobstep.result == Result.unknown
+        assert jobstep.date_started is None
+        assert jobstep.date_finished is None
+
+        resp = self.client.post(path, data={
+            'status': 'finished',
+            'result': 'passed'
+        })
+        assert resp.status_code == 200
+        data = self.unserialize(resp)
+        assert data['id'] == jobstep.id.hex
+
+        jobstep = JobStep.query.get(jobstep.id)
+
+        assert jobstep.status == Status.finished
+        assert jobstep.result == Result.passed
+        assert jobstep.date_started is not None
+        assert jobstep.date_finished is not None
