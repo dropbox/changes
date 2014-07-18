6a01eed1e09ee8f00f3a915dfb96c3e88815899c
Christopher Park <cjpark@dropbox.com>
2014-07-17 19:03:02 -0700

    Implemented DefaultBuildStep.execute (UNTESTED)

--- changes/buildsteps/default.py
+++ changes/buildsteps/default.py
@@ -1,6 +1,10 @@
 from __future__ import absolute_import
 
 from changes.buildsteps.base import BuildStep
+from changes.config import db
+from changes.constants import Status
+from changes.db.utils import get_or_create
+from changes.models import Command as CommandModel, JobPhase, JobStep
 
 
 class Command(object):
@@ -39,7 +43,49 @@ class DefaultBuildStep(BuildStep):
         return 'Build via Changes Client'
 
     def execute(self, job):
-        pass
+        job.status = Status.queued
+        db.session.add(job)
+
+        phase, created = get_or_create(JobPhase, where={
+            'job': job,
+            'label': 'Build {0}'.format(job.label),
+            'project': job.project,
+        }, defaults={
+            'status': Status.queued,
+        })
+
+        step, created = get_or_create(JobStep, where={
+            'job': phase.job,
+            'project': phase.project,
+            'phase': phase,
+            'label': '{0} #{1}'.format(job.label, job.id.hex),
+        }, defaults={
+            'status': Status.queued,
+            'data': {
+                'build_no': None,
+                'item_id': job.id.hex,
+                'queued': True,
+                'uri': None,
+            }
+        })
+
+        # if not created:
+        #     if step.date_created
+        print type(self.commands)
+        for index, command in enumerate(self.commands):
+            command_model, created = get_or_create(CommandModel, where={
+                'jobstep': step,
+                'label': '{0} #{1}'.format(job.label, index),
+                'status': Status.queued,
+                'jobstep': step,
+                'script': command.script,
+                'env': command.env,
+                'cwd': command.path,
+                'artifacts': command.artifacts
+            }, defaults={
+                'status': Status.queued,
+            })
+        db.session.commit()
 
     def update(self, job):
         pass
