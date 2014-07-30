f0ff058ce5cb1e89a3057e0ddad54dc645e4843e
David Cramer <cramer@dropbox.com>
2014-07-30 14:35:15 -0700

    Optimize timeSince directive

--- static/js/directives/timeSince.js
+++ static/js/directives/timeSince.js
@@ -1,35 +1,24 @@
 define(['app', 'utils'], function(app, utils) {
   'use strict';
 
+  // we use a shared timeout for the page loop to avoid
+  // extremely large amounts of $digest cycles and wasteful
+  // amounts of defers
+  window.setTimeout(function(){
+    $('.ng-timesince').each(function(_, element){
+      var $element = angular.element(element),
+          value = $element.data('datetime');
+
+      $element.text(utils.time.timeSince(value));
+    });
+  }, 1000);
+
   app.directive('timeSince', ['$timeout', function($timeout) {
     return function timeSince(scope, element, attrs) {
-      var $element = $(element),
-          timeout_id;
-
-      function tick(){
-        var value = scope.$eval(attrs.timeSince),
-            age = Math.abs(value / 1000 / 60),
-            seconds_until_update;
-
-        element.text(utils.time.timeSince(value));
-
-        if (age < 1) {
-          seconds_until_update = 1;
-        } else if (age < 60) {
-          seconds_until_update = 30;
-        } else if (age < 180) {
-          seconds_until_update = 300;
-        } else {
-          seconds_until_update = 3600;
-        }
-        timeout_id = $timeout(tick, seconds_until_update * 1000);
-      }
-
-      element.bind('$destroy', function() {
-        $timeout.cancel(timeout_id);
-      });
-
-      tick();
+      var value = scope.$eval(attrs.timeSince);
+      element.addClass('ng-timesince');
+      element.data('datetime', value);
+      element.text(utils.time.timeSince(value));
     };
   }]);
 });
