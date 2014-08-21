ea417f6c17c9559df39a1be6edfa309497fc4b6b
Vishal Arora <vishal@dropbox.com>
2014-08-21 15:30:22 -0700

    Add filtering support for branch names to the API
    Test Plan: make test
    
    Reviewers: cramer
    
    Reviewed By: cramer
    
    Maniphest Tasks: T10537
    
    Differential Revision: https://tails.corp.dropbox.com/D61944

--- changes/api/repository_tree_index.py
+++ changes/api/repository_tree_index.py
@@ -1,5 +1,10 @@
 from __future__ import absolute_import, division, unicode_literals
 
+import re
+
+from flask.ext.restful import reqparse
+from sqlalchemy.sql import func
+
 from changes.api.base import APIView
 from changes.models import Repository
 
@@ -10,6 +15,11 @@ class RepositoryTreeIndexAPIView(APIView):
     This is exposed as an API on the repository because in the future there may
     be related preferences that affect the behavior for associated trees.
     """
+    TREE_ARGUMENT_NAME = 'tree'
+
+    get_parser = reqparse.RequestParser()
+    get_parser.add_argument(TREE_ARGUMENT_NAME, type=unicode, location='args')
+
     def get(self, repository_id):
         repo = Repository.query.get(repository_id)
         if repo is None:
@@ -19,6 +29,16 @@ class RepositoryTreeIndexAPIView(APIView):
         if not vcs:
             return {'error': 'Repository has no backend specified'}, 422
 
-        branch_name_list = vcs.get_known_branches()
-        branches = [{'name': branch_name} for branch_name in branch_name_list]
+        # Fetch all the branches
+        branch_names = vcs.get_known_branches()
+
+        # Filter out any branches that don't match the tree query
+        args = self.get_parser.parse_args()
+        if args and args[self.TREE_ARGUMENT_NAME]:
+            query = re.compile(args[self.TREE_ARGUMENT_NAME], re.IGNORECASE)
+            branches = [{'name': branch_name} for branch_name in branch_names
+                        if query.match(branch_name)]
+        else:
+            branches = [{'name': branch_name} for branch_name in branch_names]
+
         return self.paginate(branches)
--- tests/changes/api/test_repository_tree_index.py
+++ tests/changes/api/test_repository_tree_index.py
@@ -1,7 +1,8 @@
+from mock import patch
+
 from changes.testutils import APITestCase
 from changes.models import RepositoryBackend
-
-from mock import patch
+from changes.api.repository_tree_index import RepositoryTreeIndexAPIView
 
 
 class RepositoryTreeListTest(APITestCase):
@@ -40,3 +41,20 @@ class RepositoryTreeListTest(APITestCase):
         assert len(data) == 2
         self.assertIn(data[0]['name'], test_branches)
         self.assertIn(data[1]['name'], test_branches)
+
+
+    @patch('changes.vcs.git.GitVcs.get_known_branches')
+    def test_get_with_tree_filter(self, git_vcs_mock):
+        test_branches = ['master', 'MATCH_ME']
+        git_vcs_mock.return_value = test_branches
+        repo = self.create_repo(url='https://example.co.nonexistent/bar',
+                                backend=RepositoryBackend.git)
+
+        path = '/api/0/repositories/{0}/trees/?{1}={2}'.format(
+            repo.id, RepositoryTreeIndexAPIView.TREE_ARGUMENT_NAME, 'match')
+
+        resp = self.client.get(path)
+        self.assertEquals(resp.status_code, 200, resp.data)
+        data = self.unserialize(resp)
+        assert len(data) == 1
+        self.assertIn(data[0]['name'], 'MATCH_ME')
