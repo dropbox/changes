af1914d1f4609a63581f373bd71d2f1c21e04b8d
David Cramer <cramer@dropbox.com>
2014-09-16 12:25:32 -0700

    Add search to user list

--- static/js/states/adminUserList.js
+++ static/js/states/adminUserList.js
@@ -3,15 +3,41 @@ define([
 ], function(app) {
   'use strict';
 
+  function getEndpoint(params) {
+    var endpoint = '/api/0/users/?';
+
+    if (params.query !== null) {
+      endpoint += '&query=' + params.query;
+    }
+
+    if (params.sort !== null) {
+      endpoint += '&sort=' + params.sort;
+    }
+
+    if (params.per_page !== null) {
+      endpoint += '&per_page=' + params.per_page;
+    }
+
+    return endpoint;
+  }
+
   return {
     parent: 'admin_layout',
-    url: 'users/',
+    url: 'users/?query&per_page&sort',
     templateUrl: 'partials/admin/user-list.html',
-    controller: function($scope, Collection, Paginator) {
+    controller: function($scope, $state, $stateParams, Collection, Paginator) {
       var collection = new Collection();
-      var paginator = new Paginator('/api/0/users/', {
+      var paginator = new Paginator(getEndpoint($stateParams), {
         collection: collection
       });
+
+      $scope.searchForm = {
+        query: $stateParams.query
+      };
+
+      $scope.search = function(){
+        $state.go('admin_user_list', $scope.searchForm);
+      };
 
       $scope.userList = collection;
       $scope.userPaginator = paginator;
