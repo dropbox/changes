937abf5531cfa7a7503b5f2df4a2d681eba0fb19
David Cramer <cramer@dropbox.com>
2014-09-23 11:37:46 -0700

    Revert "Revert "Improve status updates to support allocated values""
    This reverts commit 7963e89c5e04c4fafaf0dc6bf7e3b8ce8ae85241.

--- changes/jobs/sync_job.py
+++ changes/jobs/sync_job.py
@@ -53,15 +53,14 @@ def sync_phase(phase):
         if all(s.status == Status.finished for s in phase_steps):
             phase.status = Status.finished
             phase.date_finished = safe_agg(max, (s.date_finished for s in phase_steps))
-
-        elif any(s.status != Status.finished for s in phase_steps):
-            phase.status = Status.in_progress
+        else:
+            phase.status = safe_agg(min, (s.status for s in phase_steps))
 
         if any(s.result is Result.failed for s in phase_steps):
             phase.result = Result.failed
 
         elif phase.status == Status.finished:
-            phase.result = safe_agg(max, (s.result for s in phase.steps))
+            phase.result = safe_agg(max, (s.result for s in phase_steps))
 
     if db.session.is_modified(phase):
         phase.date_modified = datetime.utcnow()
@@ -140,10 +139,8 @@ def sync_job(job_id):
 
     if is_finished:
         job.status = Status.finished
-    elif any(j.status is not Status.queued for j in all_phases):
-        job.status = Status.in_progress
     else:
-        job.status = Status.queued
+        job.status = safe_agg(min, (j.status for j in all_phases))
 
     if db.session.is_modified(job):
         job.date_modified = datetime.utcnow()
