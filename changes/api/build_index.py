9f9da3c32e06c78749a66498c593bcc82ec4c6ba
Vishal Arora <vishal@dropbox.com>
2014-10-06 10:20:12 -0700

    Behave the same way with invalid repository callsign as with project and repositiory parameters
    Reviewers: cramer
    
    Reviewed By: cramer
    
    Maniphest Tasks: T18551
    
    Differential Revision: https://tails.corp.dropbox.com/D69064

--- changes/api/build_index.py
+++ changes/api/build_index.py
@@ -206,6 +206,8 @@ def get_repository_by_callsign(callsign):
     ))
     if len(repo_list) > 1:
         logging.warning('Multiple repositories found matching phabricator.callsign=%s', callsign)
+    elif len(repo_list) < 1:
+        return None  # Match behavior of project and repository parameters
     return repo_list[0]
 
 
