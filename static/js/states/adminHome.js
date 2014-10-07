26742b0534c27cb9c615c182a867afa191d06bc2
David Cramer <cramer@dropbox.com>
2014-10-07 13:56:11 -0700

    Add polling for system stats

--- static/js/states/adminHome.js
+++ static/js/states/adminHome.js
@@ -8,16 +8,30 @@ define([
     url: '',
     templateUrl: 'partials/admin/home.html',
     controller: function($scope, $http, flash) {
+      var timeoutId;
+
       $scope.loading = true;
 
-      $http.get('/api/0/systemstats/').success(function(data){
-        $scope.statusCounts = data.statusCounts;
-        $scope.resultCounts = data.resultCounts;
-      }).error(function(){
-        flash('error', 'There was an error loading system statistics.');
-      }).finally(function(){
-        $scope.loading = false;
+      $scope.$on('$destory', function(){
+        if (timeoutId) {
+          window.cancelTimeout(timeoutId);
+        }
       });
+
+      function tick() {
+        $http.get('/api/0/systemstats/').success(function(data){
+          $scope.statusCounts = data.statusCounts;
+          $scope.resultCounts = data.resultCounts;
+        }).error(function(){
+          flash('error', 'There was an error loading system statistics.');
+        }).finally(function(){
+          $scope.loading = false;
+        });
+
+        timeoutId = window.setTimeout(tick, 5000);
+      }
+
+      tick();
     }
   };
 });
