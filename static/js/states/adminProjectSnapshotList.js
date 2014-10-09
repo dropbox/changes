7dda9f20850c360b52c4d23bf02665dd6cbd45cb
David Cramer <cramer@dropbox.com>
2014-10-09 13:33:31 -0700

    Add modal for showing snapshot images

--- static/js/states/adminProjectSnapshotList.js
+++ static/js/states/adminProjectSnapshotList.js
@@ -3,11 +3,16 @@ define([
 ], function(app) {
   'use strict';
 
+  app.controller('ProjectSnapshotDetailsCtrl', function($scope, snapshotData) {
+    $scope.snapshot = snapshotData;
+    $scope.imageList = snapshotData.images;
+  });
+
   return {
     parent: 'admin_project_details',
     url: 'snapshots/',
     templateUrl: 'partials/admin/project-snapshot-list.html',
-    controller: function($http, $scope, $stateParams, Collection, Paginator, projectData) {
+    controller: function($http, $modal, $scope, $state, $stateParams, Collection, Paginator, projectData) {
       var collection = new Collection();
       var paginator = new Paginator('/api/0/projects/' + $stateParams.project_id + '/snapshots/', {
         collection: collection
@@ -26,6 +31,18 @@ define([
           'snapshot.current': '',
         }).success(function(){
           $scope.currentSnapshotId = '';
+        });
+      };
+
+      $scope.showDetails = function(snapshot) {
+        var modalInstance = $modal.open({
+          templateUrl: 'partials/admin/project-snapshot-details.html',
+          controller: 'ProjectSnapshotDetailsCtrl',
+          resolve: {
+            snapshotData: function() {
+              return snapshot;
+            }
+          }
         });
       };
 
