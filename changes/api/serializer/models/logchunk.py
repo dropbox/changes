5ad8be7904b18bccd4f8c97cbfeecbce9c590d15
David Cramer <cramer@dropbox.com>
2014-11-18 13:07:37 -0800

    Gracefully handle unparseable ansi

--- changes/api/serializer/models/logchunk.py
+++ changes/api/serializer/models/logchunk.py
@@ -1,5 +1,8 @@
 from ansi2html import Ansi2HTMLConverter
 
+from flask import current_app
+from werkzeug.utils import escape
+
 from changes.api.serializer import Serializer, register
 from changes.models.log import LogChunk
 
@@ -8,7 +11,11 @@ from changes.models.log import LogChunk
 class LogChunkSerializer(Serializer):
     def serialize(self, instance, attrs):
         conv = Ansi2HTMLConverter()
-        formatted_text = conv.convert(instance.text, full=False)
+        try:
+            formatted_text = conv.convert(instance.text, full=False)
+        except Exception:
+            current_app.logger.exception('Unable to convert ansi to html: %s', instance.text)
+            formatted_text = escape(instance.text)
 
         return {
             'id': instance.id.hex,
