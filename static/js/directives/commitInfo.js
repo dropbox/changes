d1ea41dcbb17cb173599ef2d7342eb99e3ac2b45
Stefan Avramov <stefan@dropbox.com>
2015-01-20 16:17:05 -0800

    Move commit-info template to its own html file
    Summary: Now we put all templates in javasript, so the bug this was trying to fix is not there anymore
    
    Reviewers: vishal
    
    Reviewed By: vishal
    
    Projects: #changes
    
    Differential Revision: https://tails.corp.dropbox.com/D84579

--- static/js/directives/commitInfo.js
+++ static/js/directives/commitInfo.js
@@ -3,24 +3,9 @@
 
   define(['app'], function(app) {
     app.directive('commitInfo', function() {
-      var templateContent = '' +
-          '<div>' +
-            '<h5><a ui-sref="build_details({project_id: commit.project.slug, build_id: commit.id})">{{title}}</a></h5>' +
-            '<div class="info">' +
-              '<span ng-if="showProject"><a ui-sref="project_builds({project_id: commit.project.slug})">{{commit.project.name}}</a> &mdash;</span>' +
-              '<span class="build-tag" ng-repeat="tag in commit.tags">{{tag}}</span>' +
-              '<a ui-sref="project_source_details({project_id: commit.project.slug, source_id: commit.source.id})">{{commit.target}}</a>' +
-              '<span ng-if="commit.author">&mdash; {{commit.author.name}}</span>' +
-              '<span ng-if="commit.stats.test_failures"> &mdash; <span style="color:red">{{commit.stats.test_failures}} test failures</span></span>' +
-            '</div>' +
-          '</div>';
-
       return {
         restrict: 'E',
-        // Workaround for a bug in Angular 1.2.1: use template instead of templateUrl.
-        //    For more details, see https://github.com/angular/angular.js/issues/2151.
-        //templateUrl: 'partials/includes/commit-info.html',
-        template: templateContent,
+        templateUrl: 'partials/directives/commit-info.html',
         scope: {
           commit: '=',
           title: '=',
@@ -30,5 +15,4 @@
       };
     });
   });
-
 })();
