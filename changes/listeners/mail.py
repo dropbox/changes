414ee85cf18143f659930da66dc6b4b46d9c72d0
Kyle Consalus <kylec@dropbox.com>
2015-04-01 14:26:32 -0700

    Make trying to resend a collection mail a warning instead of an assertion
    Summary:
    Tasks are expected to be re-run occasionally if they fail to ack for some reason,
    so issuing a warning and terminating successfully seems more appropriate than asserting
    something we don't expect to always be true.
    We've seen this assertion fail ~1k times in the past couple weeks, and that may be indicative
    of other issues, but we can track that with the warning.
    
    Test Plan: None
    
    Reviewers: kajic
    
    Reviewed By: kajic
    
    Subscribers: changesbot, wwu
    
    Differential Revision: https://tails.corp.dropbox.com/D99573

--- changes/listeners/mail.py
+++ changes/listeners/mail.py
@@ -91,8 +91,11 @@ class MailNotificationHandler(object):
                 'recipients': msg.recipients,
             }
         })
-        assert event, 'An email has already been sent for collection_id={} (build_id={}).'.format(
-            build.collection_id, build.id.hex)
+        # If we were unable to create the Event, we must've done so (and thus sent the mail) already.
+        if not event:
+            self.logger.warning('An email has already been sent for collection_id={} (build_id={}).'.format(
+                build.collection_id, build.id.hex))
+            return
 
         mail.send(msg)
 
