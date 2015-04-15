dac3d9e0fb14a42a82ca38c17988c13e4ff4df89
Josiah Boning <josiah@dropbox.com>
2015-04-14 22:51:55 -0700

    Serialize RevisionResult id as a "sha" field too.
    Summary:
    Revision object shas are serialized as both "id" and "sha", and js uses
    sha for a lot of things. The lack of a sha field leads to funny things
    like not showing a "#Start" button, and, if there are more than one
    RevisionResult objects serialized, the second one overwriting the first
    one because we compare by sha.
    
    Reviewers: vishal
    
    Reviewed By: vishal
    
    Subscribers: changesbot
    
    Differential Revision: https://tails.corp.dropbox.com/D102372

--- changes/api/serializer/models/revisionresult.py
+++ changes/api/serializer/models/revisionresult.py
@@ -7,6 +7,7 @@ class RevisionSerializer(Serializer):
     def serialize(self, instance, attrs):
         return {
             'id': instance.id,
+            'sha': instance.id,  # Having both id and sha is a bit distasteful. We should try to fix this.
             'message': instance.message,
             'author': None,  # We don't return author information
             'dateCreated': instance.author_date,
