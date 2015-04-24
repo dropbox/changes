59f37c9cdb188ab3fdf5d1bb0be3219902d6536f
Kyle Consalus <kylec@dropbox.com>
2015-04-23 17:46:46 -0700

    Report failure reasons in build analytics
    Summary:
    Failure reasons are a great way to track the kinds of failures we see over time, so
    we should report them to analytics. It'd be slightly more correct to report them
    on a per-jobstep level, but we don't yet have analytics for jobsteps, and
    summarizing at the build level isn't incorrect and is useful.
    
    Test Plan: Unit
    
    Reviewers: angie, vishal
    
    Reviewed By: vishal
    
    Subscribers: mkedia, changesbot, wwu
    
    Differential Revision: https://tails.corp.dropbox.com/D104150

--- changes/listeners/analytics_notifier.py
+++ changes/listeners/analytics_notifier.py
@@ -9,11 +9,13 @@ import logging
 import json
 import re
 import requests
+from sqlalchemy import distinct
 from datetime import datetime
 
 from flask import current_app
 
-from changes.models import Build
+from changes.config import db
+from changes.models import Build, FailureReason
 
 logger = logging.getLogger('analytics_notifier')
 
@@ -47,6 +49,24 @@ def _get_phabricator_revision_url(build):
     return None
 
 
+def _get_failure_reasons(build):
+    """Return the names of all the FailureReasons associated with a build.
+    Args:
+        build (Build): The build to return reasons for.
+    Returns:
+        list: A sorted list of the distinct FailureReason.reason values associated with
+        the build.
+    """
+    failure_reasons = [r for r, in db.session.query(
+                distinct(FailureReason.reason)
+            ).filter(
+                FailureReason.build_id == build.id
+            ).all()]
+    # The order isn't particularly meaningful; the sorting is primarily
+    # to make the same set of reasons reliably result in the same JSON.
+    return sorted(failure_reasons)
+
+
 def build_finished_handler(build_id, **kwargs):
     url = current_app.config.get('ANALYTICS_POST_URL')
     if not url:
@@ -54,6 +74,8 @@ def build_finished_handler(build_id, **kwargs):
     build = Build.query.get(build_id)
     if build is None:
         return
+
+    failure_reasons = _get_failure_reasons(build)
 
     def maybe_ts(dt):
         if dt:
@@ -76,6 +98,7 @@ def build_finished_handler(build_id, **kwargs):
         # be globally unique, whereas the id is only certain to be unique for
         # a single Phabricator instance.
         'phab_revision_url': _get_phabricator_revision_url(build),
+        'failure_reasons': failure_reasons,
     }
     if build.author:
         data['author'] = build.author.email
--- tests/changes/listeners/test_analytics_notifier.py
+++ tests/changes/listeners/test_analytics_notifier.py
@@ -5,9 +5,17 @@ from datetime import datetime
 from flask import current_app
 import mock
 
-from changes.constants import Result
+from changes.config import db
+from changes.constants import Result, Status
 from changes.testutils import TestCase
-from changes.listeners.analytics_notifier import build_finished_handler, _get_phabricator_revision_url
+from changes.models import FailureReason
+from changes.listeners.analytics_notifier import (
+        build_finished_handler, _get_phabricator_revision_url, _get_failure_reasons
+)
+
+
+def ts_to_datetime(ts):
+    return datetime.utcfromtimestamp(ts)
 
 
 class AnalyticsNotifierTest(TestCase):
@@ -37,16 +45,23 @@ class AnalyticsNotifierTest(TestCase):
         started = created + 10
         finished = started + duration
 
-        def ts_to_datetime(ts):
-            return datetime.utcfromtimestamp(ts)
-
         build = self.create_build(project, result=Result.failed, target='D1',
                                   label='Some sweet diff', duration=duration,
                                   date_created=ts_to_datetime(created), date_started=ts_to_datetime(started),
                                   date_finished=ts_to_datetime(finished))
+
+        job = self.create_job(build=build, result=Result.failed)
+        jobphase = self.create_jobphase(job)
+        jobstep = self.create_jobstep(jobphase, status=Status.finished, result=Result.failed)
+        db.session.add(FailureReason(step_id=jobstep.id, job_id=job.id, build_id=build.id, project_id=project.id,
+                                     reason='missing_tests'))
+        db.session.commit()
+
         with mock.patch('changes.listeners.analytics_notifier._get_phabricator_revision_url') as mock_get_phab:
             mock_get_phab.return_value = 'https://example.com/D1'
-            build_finished_handler(build_id=build.id.hex)
+            with mock.patch('changes.listeners.analytics_notifier._get_failure_reasons') as mock_get_failures:
+                mock_get_failures.return_value = ['aborted', 'missing_tests']
+                build_finished_handler(build_id=build.id.hex)
 
         expected_data = {
             'build_id': build.id.hex,
@@ -61,8 +76,34 @@ class AnalyticsNotifierTest(TestCase):
             'date_started': started,
             'date_finished': finished,
             'phab_revision_url': 'https://example.com/D1',
+            'failure_reasons': ['aborted', 'missing_tests'],
         }
         post_fn.assert_called_once_with(URL, expected_data)
+
+    def test_get_failure_reasons_no_failures(self):
+        project = self.create_project(name='test', slug='project-slug')
+        build = self.create_build(project, result=Result.passed, target='D1',
+                                  label='Some sweet diff')
+        self.assertEquals(_get_failure_reasons(build), [])
+
+    def test_get_failure_reasons_multiple_failures(self):
+        project = self.create_project(name='test', slug='project-slug')
+        build = self.create_build(project, result=Result.failed, target='D1',
+                                  label='Some sweet diff')
+        job = self.create_job(build=build, result=Result.failed)
+        jobphase = self.create_jobphase(job)
+        jobstep = self.create_jobstep(jobphase, status=Status.finished, result=Result.failed)
+        for reason in ('missing_tests', 'timeout', 'aborted'):
+            db.session.add(FailureReason(step_id=jobstep.id, job_id=job.id, build_id=build.id, project_id=project.id,
+                                         reason=reason))
+        jobstep2 = self.create_jobstep(jobphase, status=Status.finished, result=Result.failed)
+        for reason in ('timeout', 'insufficient_politeness'):
+            db.session.add(FailureReason(step_id=jobstep2.id, job_id=job.id, build_id=build.id, project_id=project.id,
+                                         reason=reason))
+        db.session.commit()
+
+        self.assertEquals(_get_failure_reasons(build),
+                          ['aborted', 'insufficient_politeness', 'missing_tests', 'timeout'])
 
     def test_get_phab_revision_url_diff(self):
         project = self.create_project(name='test', slug='test')
