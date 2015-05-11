bbfc1bcb632e33c8afb009801dfb4034af15e371
Li Haoyi <haoyi@dropbox.com>
2015-05-11 16:46:56 -0700

    Fix static file serving in dev by serving manually in controller
    Summary: The comment says it all. Only affects dev (`app.debug`). Since we serve files directly from Flask, all the solutions out there working around the problem via configuration in Apache or Nginx or Gunicorn or ... do not apply to us
    
    Test Plan:
    Tested manually. Now i can add/remove text from javascript files, and it doesn't break! I can see the changes immediately after refresh without weird `Unexpected token ILLEGAL` errors! Swapping back to `master`, these errors reappear immediately.
    
    In exchange loading a page is slightly slower due to downloading all the JS. I think that's ok, since the alternative is to do that annoying move-file-refresh-move-file-back dance
    
    Reviewers: tiago
    
    Reviewed By: tiago
    
    Subscribers: changesbot, mkedia, wwu, josiah
    
    Differential Revision: https://tails.corp.dropbox.com/D107907

--- changes/web/static.py
+++ changes/web/static.py
@@ -1,6 +1,7 @@
-from flask import current_app as app
+from flask import current_app as app, Response
 from flask.helpers import send_from_directory
 from flask.views import MethodView
+import mimetypes
 
 
 class StaticView(MethodView):
@@ -9,5 +10,23 @@ class StaticView(MethodView):
         self.cache_timeout = app.config['SEND_FILE_MAX_AGE_DEFAULT']
 
     def get(self, filename):
-        return send_from_directory(
-            self.root, filename, cache_timeout=self.cache_timeout)
+        # We do this in debug to work around http://stackoverflow.com/q/17460463/871202
+        # By reading the file into memory ourselves, we seem to avoid hitting that
+        # VirtualBox issue in dev. In prod, it's unchanged and we just send_from_directory
+        if app.debug:
+            full_name = self.root + "/" + filename
+
+            if filename:
+                mimetype = mimetypes.guess_type(filename)[0]
+            else:
+                mimetype = 'application/octet-stream'
+            return Response(
+                open(full_name),
+                mimetype=mimetype,
+                headers={
+                    "Content-Disposition": "filename=" + filename
+                }
+            )
+        else:
+            return send_from_directory(
+                self.root, filename, cache_timeout=self.cache_timeout)
