49b3d19a9e7a5a11302ebf0302ee4d8537094171
Tiago Madeira <tiago@dropbox.com>
2015-05-27 14:29:25 -0700

    Create test.label_sha index concurrently
    Summary: To avoid blocking other database operations, this changes `add_label_sha_index` migration to create the index concurrently.
    
    Test Plan: Tested locally.
    
    Reviewers: haoyi
    
    Reviewed By: haoyi
    
    Subscribers: mkedia, anupc
    
    Differential Revision: https://tails.corp.dropbox.com/D111353

--- migrations/versions/2f12902f7f55_add_label_sha_index.py
+++ migrations/versions/2f12902f7f55_add_label_sha_index.py
@@ -11,11 +11,28 @@ revision = '2f12902f7f55'
 down_revision = '1c537486cef7'
 
 from alembic import op
+import psycopg2
 import sqlalchemy as sa
 
 
 def upgrade():
-    op.create_index('idx_test_label_sha', 'test', ['label_sha'])
+    '''
+    We need to set isolation level to make the concurrent query run outside other transaction
+    blocks. If we don't do it, we will get an error: 'sqlalchemy.exc.InternalError:
+    (psycopg2.InternalError) <query> cannot run inside a transaction block'
+
+    At some point, when our SQLAlchemy gets updated to a version >= 0.9.9, we will be able to just
+    do op.create_index with postgresql_concurrently=True.
+    '''
+    query = "CREATE INDEX CONCURRENTLY idx_test_label_sha ON test (label_sha)"
+    connection = op.get_bind()
+    connection.connection.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT)
+    connection.execute(sa.sql.text(query))
+    connection.connection.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_READ_COMMITTED)
 
 def downgrade():
-    op.drop_index('idx_test_label_sha', 'test')
+    query = "DROP INDEX CONCURRENTLY idx_test_label_sha"
+    connection = op.get_bind()
+    connection.connection.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT)
+    connection.execute(sa.sql.text(query))
+    connection.connection.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_READ_COMMITTED)
