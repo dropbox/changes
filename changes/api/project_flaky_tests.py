3da5d29a7e304502cc3625e51fb8a015def02260
Tiago Madeira <tiago@dropbox.com>
2015-05-28 10:38:05 -0700

    Add history for flaky tests
    Summary:
    Display two graphs with the history of flaky tests in Changes UI. The first one is with the aggregated number of flakes per day; the second one is per test.
    
    Screenshot: https://www.dropbox.com/s/8e46ca0tmvghw3i/Screenshot%202015-05-27%2009.52.08.png?dl=0
    
    This diff also adds the possibility to see the flakes for any specific date by adding the parameter `date` both in Angular and API flaky tests routes.
    
    Test Plan: Tested locally (see screenshot in summary). Clicked in dates with and without flakes.
    
    Reviewers: haoyi
    
    Reviewed By: haoyi
    
    Subscribers: changesbot, mkedia
    
    Differential Revision: https://tails.corp.dropbox.com/D111240

--- changes/api/project_flaky_tests.py
+++ changes/api/project_flaky_tests.py
@@ -1,50 +1,129 @@
 from __future__ import absolute_import, division, unicode_literals
 
+from datetime import date, datetime, timedelta
 from flask.ext.restful import reqparse
+from sqlalchemy.sql import func
 
 from changes.api.base import APIView
 from changes.config import db
 from changes.models import FlakyTestStat, Project, TestCase
 
 
+CHART_DATA_LIMIT = 50
+
+
 class ProjectFlakyTestsAPIView(APIView):
     parser = reqparse.RequestParser()
+    parser.add_argument('date', type=unicode, location='args')
 
     def get(self, project_id):
         project = Project.get(project_id)
         if not project:
             return '', 404
 
-        last_date = db.session.query(
-            FlakyTestStat.date
-        ).order_by(
-            FlakyTestStat.date.desc()
-        ).limit(1).scalar()
+        args = self.parser.parse_args()
+        if args.date:
+            try:
+                query_date = datetime.strptime(args.date, '%Y-%m-%d').date()
+            except:
+                return 'Can\'t parse date "%s"' % (args.date), 500
+        else:
+            query_date = date.today() - timedelta(days=1)
 
-        query = db.session.query(
-            FlakyTestStat, TestCase
+        data = {
+            'date': str(query_date),
+            'chartData': self.get_chart_data(project_id, query_date),
+            'flakyTests': self.get_flaky_tests(project_id, query_date)
+        }
+
+        return self.respond(data, serialize=False)
+
+    def get_flaky_tests(self, project_id, query_date):
+        subquery = db.session.query(
+            FlakyTestStat,
+            TestCase
         ).join(
             TestCase
-        ).filter(
-            FlakyTestStat.date == last_date,
+        )
+
+        flakytests_query = subquery.filter(
+            FlakyTestStat.date == query_date,
             FlakyTestStat.project_id == project_id
         ).order_by(
             FlakyTestStat.flaky_runs.desc()
         )
 
-        flaky_tests = []
-        for flaky_test, last_flaky_run in query:
-            flaky_tests.append({
-                'name': last_flaky_run.short_name,
-                'package': last_flaky_run.package,
-                'hash': last_flaky_run.name_sha,
+        flaky_map = {}
+        for flaky_test, test_run in flakytests_query:
+            flaky_map[test_run.name_sha] = {
+                'name': test_run.short_name,
+                'package': test_run.package,
+                'hash': test_run.name_sha,
                 'flaky_runs': flaky_test.flaky_runs,
-                'passing_runs': flaky_test.passing_runs
+                'passing_runs': flaky_test.passing_runs,
+                'history': []
+            }
+
+        if flaky_map:
+            history_query = subquery.filter(
+                FlakyTestStat.date <= query_date,
+                FlakyTestStat.date > (query_date - timedelta(days=CHART_DATA_LIMIT)),
+                TestCase.name_sha.in_(flaky_map)
+            )
+
+            # Create dict with keys in range ]today-CHART_DATA_LIMIT, today]
+            calendar = [query_date - timedelta(days=delta) for delta in range(0, CHART_DATA_LIMIT)]
+            history = {}
+            for day in calendar:
+                history[str(day)] = {}
+
+            # Insert stats in the dict
+            for flaky_test, test_run in history_query:
+                history[str(flaky_test.date)][test_run.name_sha] = {
+                    'date': str(flaky_test.date),
+                    'flaky_runs': flaky_test.flaky_runs,
+                    'passing_runs': flaky_test.passing_runs
+                }
+
+            # For each test, generate its history array from global history dict
+            for day in reversed(sorted(history)):
+                default_data = {
+                    'date': day,
+                    'flaky_runs': 0,
+                    'passing_runs': 0
+                }
+                for sha in flaky_map:
+                    flaky_map[sha]['history'].append(history[day].get(sha, default_data))
+
+        return [flaky_map[sha] for sha in flaky_map]
+
+    def get_chart_data(self, project_id, query_date):
+        calendar = db.session.query(
+            func.generate_series(
+                query_date - timedelta(days=CHART_DATA_LIMIT - 1),
+                query_date,
+                timedelta(days=1)
+            ).label('day')
+        ).subquery()
+
+        historical_data = db.session.query(
+            calendar.c.day,
+            func.coalesce(func.sum(FlakyTestStat.flaky_runs), 0),
+            func.coalesce(func.sum(FlakyTestStat.passing_runs), 0)
+        ).outerjoin(
+            FlakyTestStat, calendar.c.day == FlakyTestStat.date
+        ).order_by(
+            calendar.c.day.desc()
+        ).group_by(
+            calendar.c.day
+        )
+
+        chart_data = []
+        for d, flaky_runs, passing_runs in historical_data:
+            chart_data.append({
+                'date': str(d.date()),
+                'flaky_runs': flaky_runs,
+                'passing_runs': passing_runs
             })
 
-        data = {
-            'lastUpdate': str(last_date),
-            'flakyTests': flaky_tests
-        }
-
-        return self.respond(data, serialize=False)
+        return chart_data
--- static/js/states/projectFlakyTests.js
+++ static/js/states/projectFlakyTests.js
@@ -3,17 +3,53 @@ define([
 ], function(app) {
     'use strict';
 
+    var CHART_DATA_LIMIT = 50;
+
     return {
         parent: 'project_details',
-        url: 'flaky_tests/',
+        url: 'flaky_tests/?date',
         templateUrl: 'partials/project-flaky-tests.html',
-        controller: function($scope, projectData, flakyTestsData) {
-            $scope.lastUpdate = flakyTestsData.lastUpdate;
+        controller: function($scope, $state, projectData, flakyTestsData) {
+            var genChartData = function(data) {
+                return {
+                    data: data.map(function(day) {
+                        return {
+                            className: day.flaky_runs > 0 ? 'result-failed' : 'result-passed',
+                            value: day.flaky_runs,
+                            data: day
+                        };
+                    }),
+                    options: {
+                        limit: CHART_DATA_LIMIT,
+                        linkFormatter: function(item) {
+                            return $state.href('project_flaky_tests', {date: item.date});
+                        },
+                        tooltipFormatter: function(item) {
+                            var ratio = 0;
+                            if (item.passing_runs > 0) {
+                                ratio = (100 * item.flaky_runs/item.passing_runs).toFixed(2);
+                            }
+                            return '<h5>' + item.date + '</h5>' +
+                                '<p>Flaky runs: ' + item.flaky_runs +
+                                ' (' + ratio + '% of passing runs)</p>';
+                        }
+                     }
+                };
+            };
+
+            $scope.chartData = genChartData(flakyTestsData.chartData);
+
+            flakyTestsData.flakyTests.map(function(test) {
+                test.chartData = genChartData(test.history);
+            });
             $scope.flakyTests = flakyTestsData.flakyTests;
+
+            $scope.date = flakyTestsData.date;
         },
         resolve: {
             flakyTestsData: function($http, $stateParams, projectData) {
-                var url = '/api/0/projects/' + projectData.id + '/flaky_tests/';
+                var url = '/api/0/projects/' + projectData.id + '/flaky_tests/?date=' +
+                    ($stateParams.date || '');
                 return $http.get(url).then(function(response) {
                     return response.data;
                 });
