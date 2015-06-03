09b5d390634aa9c6d74ccc4a3eca823411dc9eb3
Li Haoyi <haoyi@dropbox.com>
2015-06-03 12:00:48 -0700

    Start logging build metrics
    Summary: Send them to our metrics endpoint so we can put them in hive
    
    Test Plan:
    Not tested, basically copied verbatim from
    
    https://mirth.corp.dropbox.com/file/vmsetup/python/lib/provisioner.py#L40
    
    Reviewers: tiago
    
    Reviewed By: tiago
    
    Subscribers: changesbot, mkedia, wwu
    
    Differential Revision: https://tails.corp.dropbox.com/D112845

--- changes/jobs/flaky_tests.py
+++ changes/jobs/flaky_tests.py
@@ -10,6 +10,23 @@ from changes.config import db
 from changes.db.utils import try_create
 from changes.lib.flaky_tests import get_flaky_tests
 from changes.models import FlakyTestStat, Project
+import urllib2
+
+
+def log_metrics(key, **kws):
+    try:
+
+        urllib2.urlopen(
+            "https://www.dropbox.com/build_metrics" +
+            "?key=%s" % key +
+            "".join(
+                "&%s=%s" % (urllib2.quote(str(k)), urllib2.quote(str(v)))
+                for (k, v) in kws.items()
+            )
+        ).read()
+    except Exception as e:
+        print("Logging Failed", e)
+        pass
 
 
 def aggregate_flaky_tests(day=None, max_flaky_tests=200):
@@ -23,6 +40,13 @@ def aggregate_flaky_tests(day=None, max_flaky_tests=200):
             tests = get_flaky_tests(day, day + timedelta(days=1), [project], max_flaky_tests)
 
             for test in tests:
+                log_metrics(
+                    "flaky_test_reruns",
+                    flaky_test_reruns_name=test['name'],
+                    flaky_test_reruns_project_id=test['project_id'],
+                    flaky_test_reruns_flaky_runs=test['flaky_runs'],
+                    flaky_test_reruns_passing_runs=test['passing_runs'],
+                )
                 try_create(FlakyTestStat, {
                     'name': test['name'],
                     'project_id': test['project_id'],
