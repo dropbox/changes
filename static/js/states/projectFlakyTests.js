9d4880dd6e309317417fc54a84bdd9d8aabda153
Tiago Madeira <tiago@dropbox.com>
2015-06-09 14:18:24 -0700

    Plot relative value of flaky over passing runs instead of flaky runs absolute value
    Summary:
    Plot relative value of flaky runs over passing runs instead of flaky runs absolute value, so it's easier to tell if the flaky runs are going up or down looking for the chart.
    
    If there are less than 50 passing runs, we don't plot anything to avoid creating fake peaks in the weekends.
    
    Test Plan:
    Tested locally with different `MINIMUM_PASSING_RUNS` value.
    
    Screenshot: https://www.dropbox.com/s/fs22d5gr3hoqotm/Screenshot%202015-06-08%2011.47.21.png?dl=0
    
    Reviewers: haoyi
    
    Reviewed By: haoyi
    
    Subscribers: changesbot, mkedia
    
    Differential Revision: https://tails.corp.dropbox.com/D113883

--- static/js/states/projectFlakyTests.js
+++ static/js/states/projectFlakyTests.js
@@ -4,23 +4,34 @@ define([
     'use strict';
 
     var CHART_DATA_LIMIT = 50;
+    var MINIMUM_PASSING_RUNS = 50;
 
     return {
         parent: 'project_details',
         url: 'flaky_tests/?date',
         templateUrl: 'partials/project-flaky-tests.html',
         controller: function($scope, $state, projectData, flakyTestsData) {
-            var genChartData = function(data) {
+            var genChartData = function(data, isOverallGraph) {
                 return {
                     data: data.map(function(day) {
                         var className = 'result-unknown';
-                        if (data.isOverallGraph || day.test_existed) {
+                        if (isOverallGraph || day.test_existed) {
                             className = day.flaky_runs > 0 ? 'result-failed' : 'result-passed';
+                        }
+
+                        var value = day.flaky_runs;
+                        if (isOverallGraph) {
+                            if (day.passing_runs < MINIMUM_PASSING_RUNS) {
+                                className = 'result-unknown';
+                                value = 0;
+                            } else {
+                                value = 100 * day.flaky_runs / day.passing_runs;
+                            }
                         }
 
                         return {
                             className: className,
-                            value: day.flaky_runs,
+                            value: value,
                             data: day
                         };
                     }),
@@ -30,11 +41,16 @@ define([
                             return $state.href('project_flaky_tests', {date: item.date});
                         },
                         tooltipFormatter: function(item) {
-                            var ratio = 0;
+                            var ratio = 0, extra_msg = '';
                             if (item.passing_runs > 0) {
                                 ratio = (100 * item.flaky_runs/item.passing_runs).toFixed(2);
                             }
-                            return '<h5>' + item.date + '</h5>' +
+                            if (isOverallGraph && item.passing_runs < MINIMUM_PASSING_RUNS) {
+                                extra_msg = '<p>The ratio of flaky runs on this date is not' +
+                                    ' plotted because its flaky tests had less than ' +
+                                    MINIMUM_PASSING_RUNS + ' passing runs.</p>';
+                            }
+                            return '<h5>' + item.date + '</h5>' + extra_msg +
                                 '<p>Flaky runs: ' + item.flaky_runs +
                                 ' (' + ratio + '% of passing runs)<br />' +
                                 'Double flakes: ' + item.double_reruns + '</p>';
@@ -45,11 +61,10 @@ define([
 
             // We set isOverallGraph so genChartData can differentiate individual test
             // history graph from the overall graph
-            flakyTestsData.chartData.isOverallGraph = true;
-            $scope.chartData = genChartData(flakyTestsData.chartData);
+            $scope.chartData = genChartData(flakyTestsData.chartData, true);
 
             flakyTestsData.flakyTests.map(function(test) {
-                test.chartData = genChartData(test.history);
+                test.chartData = genChartData(test.history, false);
 
                 // We create an element and get its outerHTML to escape the output.
                 var text = document.createTextNode(test.output);
