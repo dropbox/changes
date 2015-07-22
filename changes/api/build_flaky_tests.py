7fddc182b3bdceace45244d14356fe047f209840
Tiago Madeira <tiago@dropbox.com>
2015-07-22 14:30:31 -0700

    Return test author and captured output in build flaky tests API call
    Summary: This will be used by Quarantine Keeper to assign the task to fix a flaky test to its author.
    
    Test Plan:
    Tested locally. API response:
    
    ```
    {
      "projectSlug": "changesjenkins",
      "repositoryUrl": "https://github.com/dropbox/changes.git",
      "flakyTests": {
        "count": 1,
        "items": [
          {
            "captured_output": "\n            Dummy System Output!\n        ",
            "name": "tests.account.test_account.AccountTest.test_account_change_language",
            "author": {
              "email": "tiago@dropbox.com"
            }
          }
        ]
      }
    }
    ```
    
    Reviewers: haoyi
    
    Reviewed By: haoyi
    
    Subscribers: changesbot, mkedia
    
    Differential Revision: https://tails.corp.dropbox.com/D124770

--- changes/api/build_flaky_tests.py
+++ changes/api/build_flaky_tests.py
@@ -1,9 +1,15 @@
 from __future__ import absolute_import
 
+from sqlalchemy.orm import joinedload
+
 from changes.api.base import APIView
 from changes.config import db
 from changes.constants import Result
 from changes.models import Build, Job, TestCase
+
+
+# This constant must match MAX_TESTS_TO_ADD in citools' quarantine keeper
+MAX_TESTS_TO_ADD = 2
 
 
 class BuildFlakyTestsAPIView(APIView):
@@ -17,14 +23,40 @@ class BuildFlakyTestsAPIView(APIView):
         ))
 
         flaky_tests_query = db.session.query(
-            TestCase.name
+            TestCase.name,
+            TestCase.name_sha,
+            TestCase.message
         ).filter(
             TestCase.job_id.in_([j.id for j in jobs]),
             TestCase.result == Result.passed,
             TestCase.reruns > 1
-        ).order_by(TestCase.name.asc())
+        ).order_by(TestCase.name.asc()).all()
 
-        flaky_tests = map(lambda test: {'name': test.name}, flaky_tests_query)
+        flaky_tests = []
+        for test in flaky_tests_query:
+            item = {
+                'name': test.name,
+                'captured_output': test.message,
+            }
+
+            # Quarantine Keeper only needs the author if there are at most
+            # MAX_TESTS_TO_ADD to add. If there are less, it will only send
+            # an alert and we don't want to waste time querying the DB
+            if len(flaky_tests_query) <= MAX_TESTS_TO_ADD:
+                first_test = TestCase.query.filter(
+                    TestCase.project_id == build.project_id,
+                    TestCase.name_sha == test.name_sha,
+                ).order_by(TestCase.date_created.asc()).limit(1).first()
+
+                first_build = Build.query.options(
+                    joinedload('author'),
+                ).filter(
+                    Build.id == first_test.job.build_id,
+                ).first()
+
+                item['author'] = {'email': first_build.author.email}
+
+            flaky_tests.append(item)
 
         context = {
             'projectSlug': build.project.slug,
