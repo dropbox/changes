88f3f67bb0c737a61f45d727710c31a633c26472
Paul Ruan <paulruan@dropbox.com>
2015-07-30 17:17:59 -0700

    Replacing window.cancelTimeout with clearTimeout
    Summary: window.cancelTimeout doesn't exist.
    
    Test Plan:
    Verified in Chrome console that clearTimeout exists
    but not window.cancelTimeout
    
    Reviewers: mkedia, haoyi, kylec
    
    Reviewed By: kylec
    
    Subscribers: changesbot, mkedia
    
    Differential Revision: https://tails.corp.dropbox.com/D127585

--- static/js/states/adminHome.js
+++ static/js/states/adminHome.js
@@ -14,7 +14,7 @@ define([
 
       $scope.$on('$destroy', function(){
         if (timeoutId) {
-          window.cancelTimeout(timeoutId);
+          clearTimeout(timeoutId);
         }
       });
 
