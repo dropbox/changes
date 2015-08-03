38d8ebc271c020efb9b59c2edb2c5f8fbeb985c5
Mihir Kedia <mkedia@dropbox.com>
2015-08-03 15:40:44 -0700

    Refactor project_commit_index
    Summary: Easier to read/understand
    
    Test Plan: unit test
    
    Reviewers: haoyi, kylec
    
    Reviewed By: kylec
    
    Subscribers: changesbot, mkedia
    
    Differential Revision: https://tails.corp.dropbox.com/D126151 #skipthequeue

--- changes/api/project_commit_index.py
+++ changes/api/project_commit_index.py
@@ -26,111 +26,47 @@ class ProjectCommitIndexAPIView(APIView):
     def get(self, project_id):
         project = Project.get(project_id)
         if not project:
-            return '', 404
+            return error('project not found', http_code=404)
 
         args = self.get_parser.parse_args()
 
-        repo = project.repository
-        vcs = repo.get_vcs()
-
-        offset = (args.page - 1) * args.per_page
-        limit = args.per_page + 1
-
+        # we want to only return commits in the repo that are within the
+        # project's whitelist
         paths = None
         if not args.every_commit:
-            whitelist = db.session.query(
-                ProjectOption.project_id, ProjectOption.name, ProjectOption.value
-            ).filter(
-                ProjectOption.project_id.in_([project.id]),
-                ProjectOption.name.in_(['build.file-whitelist'])
-            ).first()
+            paths = self.get_whitelisted_paths(project)
 
-            if whitelist:
-                paths = whitelist.value.strip().splitlines()
+        repo = project.repository
+        offset = (args.page - 1) * args.per_page
+        limit = args.per_page + 1  # +1 to tell if there are more revs to get
 
+        vcs = repo.get_vcs()
         if vcs:
-            try:
-                vcs_log = list(vcs.log(
-                    offset=offset,
-                    limit=limit,
-                    parent=args.parent,
-                    branch=args.branch,
-                    paths=paths
-                ))
-            except ValueError as err:
-                return error(err.message)
-
-            if vcs_log:
-                revisions_qs = list(Revision.query.options(
-                    joinedload('author'),
-                ).filter(
-                    Revision.repository_id == repo.id,
-                    Revision.sha.in_(c.id for c in vcs_log)
-                ))
-
-                revisions_map = dict(
-                    (c.sha, d)
-                    for c, d in itertools.izip(revisions_qs, self.serialize(revisions_qs))
-                )
-
-                commits = []
-                for commit in vcs_log:
-                    if commit.id in revisions_map:
-                        result = revisions_map[commit.id]
-                    else:
-                        result = self.serialize(commit)
-                    commits.append(result)
-            else:
-                commits = []
-        elif args.parent or args.branch:
-            param = 'Branches' if args.branch else 'Parents'
-            return error('{0} not supported for projects with no repository.'.format(param),
-                         http_code=422)
+            commits = self.get_commits_from_vcs(
+                repo, vcs, offset, limit, paths, args.parent, args.branch)
         else:
-            commits = self.serialize(list(
-                Revision.query.options(
-                    joinedload('author'),
-                ).filter(
-                    Revision.repository_id == repo.id,
-                ).order_by(Revision.date_created.desc())[offset:offset + limit]
-            ))
+            if args.parent or args.branch:
+                param = 'Branches' if args.branch else 'Parents'
+                return error(
+                    '{0} not supported for projects with no repository.'.format(param),
+                    http_code=422)
+            # TODO: right now this fallback returns every commit for projects
+            # with whitelisted paths.  At the very least, we need to tell the
+            # frontend about this (perhaps using a response header)
+            commits = self.get_commits_from_db(repo, offset, limit)
 
         page_links = self.make_links(
             current_page=args.page,
             has_next_page=len(commits) > args.per_page,
         )
-
+        # we fetched one extra commit so that we'd know whether to create a
+        # next link. Delete it
         commits = commits[:args.per_page]
 
+        builds_map = {}
         if commits:
-            builds_qs = list(Build.query.options(
-                joinedload('author'),
-                contains_eager('source'),
-            ).join(
-                Source, Source.id == Build.source_id,
-            ).filter(
-                Build.source_id == Source.id,
-                Build.project_id == project.id,
-                Build.status.in_([Status.finished, Status.in_progress, Status.queued]),
-                Build.cause != Cause.snapshot,
-                Source.repository_id == project.repository_id,
-                Source.revision_sha.in_(c['id'] for c in commits),
-                Source.patch == None,  # NOQA
-            ).order_by(Build.date_created.asc()))
-
-            if not args.all_builds:
-                # this implicitly only keeps the last build for a revision
-                builds_map = dict(
-                    (b.source.revision_sha, d)
-                    for b, d in itertools.izip(builds_qs, self.serialize(builds_qs))
-                )
-            else:
-                builds_map = defaultdict(list)
-                for b, d in itertools.izip(builds_qs, self.serialize(builds_qs)):
-                    builds_map[b.source.revision_sha].append(d)
-
-        else:
-            builds_map = {}
+            builds_map = self.get_builds_for_commit(
+                commits, project, args.all_builds)
 
         results = []
         for result in commits:
@@ -138,7 +74,92 @@ class ProjectCommitIndexAPIView(APIView):
                 result['builds'] = builds_map.get(result['id'], [])
             else:
                 result['build'] = builds_map.get(result['id'])
-
             results.append(result)
 
         return self.respond(results, serialize=False, links=page_links)
+
+    def get_whitelisted_paths(self, project):
+        whitelist = db.session.query(
+            ProjectOption.project_id, ProjectOption.name, ProjectOption.value
+        ).filter(
+            ProjectOption.project_id.in_([project.id]),
+            ProjectOption.name.in_(['build.file-whitelist'])
+        ).first()
+
+        if whitelist:
+            paths = whitelist.value.strip().splitlines()
+        return None
+
+    def get_commits_from_vcs(self, repo, vcs, offset, limit, paths, parent, branch):
+        vcs_log = None
+        try:
+            vcs_log = list(vcs.log(
+                offset=offset,
+                limit=limit,
+                parent=parent,
+                branch=branch,
+                paths=paths
+            ))
+        except ValueError as err:
+            return error(err.message)
+
+        if not vcs_log:
+            return []
+
+        revisions_qs = list(Revision.query.options(
+            joinedload('author'),
+        ).filter(
+            Revision.repository_id == repo.id,
+            Revision.sha.in_(c.id for c in vcs_log)
+        ))
+
+        revisions_map = dict(
+            (c.sha, d)
+            for c, d in itertools.izip(revisions_qs, self.serialize(revisions_qs))
+        )
+
+        commits = []
+        for commit in vcs_log:
+            if commit.id in revisions_map:
+                result = revisions_map[commit.id]
+            else:
+                result = self.serialize(commit)
+            commits.append(result)
+        return commits
+
+    def get_commits_from_db(self, repo, offset, limit):
+        return self.serialize(list(
+            Revision.query.options(
+                joinedload('author'),
+            ).filter(
+                Revision.repository_id == repo.id,
+            ).order_by(Revision.date_created.desc())[offset:offset + limit]
+        ))
+
+    def get_builds_for_commit(self, commits, project, all_builds):
+        builds_qs = list(Build.query.options(
+            joinedload('author'),
+            contains_eager('source'),
+        ).join(
+            Source, Source.id == Build.source_id,
+        ).filter(
+            Build.source_id == Source.id,
+            Build.project_id == project.id,
+            Build.status.in_([Status.finished, Status.in_progress, Status.queued]),
+            Build.cause != Cause.snapshot,
+            Source.repository_id == project.repository_id,
+            Source.revision_sha.in_(c['id'] for c in commits),
+            Source.patch == None,  # NOQA
+        ).order_by(Build.date_created.asc()))
+
+        if not all_builds:
+            # this implicitly only keeps the last build for a revision
+            return dict(
+                (b.source.revision_sha, d)
+                for b, d in itertools.izip(builds_qs, self.serialize(builds_qs))
+            )
+        else:
+            builds_map = defaultdict(list)
+            for b, d in itertools.izip(builds_qs, self.serialize(builds_qs)):
+                builds_map[b.source.revision_sha].append(d)
+            return dict(builds_map)
