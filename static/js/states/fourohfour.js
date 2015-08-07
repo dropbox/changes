bbf4f73a32e9f2cf3b87ea9b0f571dbd380fe2c6
Mihir Kedia <mkedia@dropbox.com>
2015-08-07 15:38:39 -0700

    keep the original failed url in the url bar on 404
    Summary: make it easy to fix typos
    
    Test Plan: wrote a garbage 404 url in url bar
    
    Reviewers: haoyi, josiah
    
    Reviewed By: josiah
    
    Subscribers: changesbot, mkedia
    
    Differential Revision: https://tails.corp.dropbox.com/D129806 #skipthequeue

--- static/js/states/fourohfour.js
+++ static/js/states/fourohfour.js
@@ -6,10 +6,15 @@ define([
   return {
     url: '/404/:original_url/',
     templateUrl: 'partials/fourohfour.html',
-    controller: function($scope, $location, $stateParams) {
+    controller: function($scope, $location, $stateParams, $window) {
       // see comment in routes.js about using base64 encoding
       $scope.original_url = atob(
         $stateParams.original_url.replace("-", "/")
+      );
+      $window.history.replaceState(
+        null,
+        '404 original url',
+        $scope.original_url
       );
     }
   };
