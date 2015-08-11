05bb0495e646e9a4e13f5fbef8657dd3e8252c14
Mihir Kedia <mkedia@dropbox.com>
2015-08-11 11:09:51 -0700

    Batch sql queries in the serializer
    Summary:
    In the status quo, there's basically one place where we need to run
    logic to batch multiple data fetches together: when we have a list of objects
    with serializers. I wrote a function that will greedily look at the items in
    each of these serialized dictionaries, and do a kind of parallel depth-first
    traversal, e.g.
    
    The function is passed a list of serialized builds (dicts). It finds the first
    key/value pair that needs serialization, e.g. 'source': Source. It serializes
    all of them together, resulting in a list of serialized sources. We can
    recursively run ourselves on that list to serialize things like the revision
    child of source in parallel (which does data fetching.) Now we modify our list
    of serialized builds, replacing the source attributes with the serialized
    sources we've just run. And return the list of serialized builds.
    
    Quips -
    * And people say interview questions have no relation to actual coding
    * @kylec, I put you as a reviewer. This is the best welcome back gift I could
      think of :P
    
    Test Plan: arc test. Also, `/api/0/projects/?__trace__=1` is batched!
    
    Reviewers: haoyi, kylec
    
    Reviewed By: kylec
    
    Subscribers: changesbot, mkedia, kylec, jhance, josiah
    
    Differential Revision: https://tails.corp.dropbox.com/D122261

--- tests/changes/api/serializer/test_base.py
+++ tests/changes/api/serializer/test_base.py
@@ -1,4 +1,4 @@
-from changes.api.serializer import serialize
+from changes.api.serializer import serialize, Crumbler
 
 
 def test_identity():
@@ -11,6 +11,87 @@ def test_identity():
         ["one", 2, 3],
         {'x': 11, 'y': 22},
         {'x': ['yes', 'no'], 'y': {'k': [1, 2, 3]}}
-        ]
+    ]
     for val in passthrough:
         assert serialize(val) == val
+
+
+def test_greedy_fetching():
+    """
+    Test that our greedy algorithm does what we expect. Hopefully, all of the
+    other tests will serve to verify that it doesn't mess anything up, so we only
+    need to make sure that it actually works
+    """
+
+    class Parent(object):
+        def __init__(self, data):
+            self.data = data
+
+    class Child(Parent):
+        pass
+
+    class Baby(Child):
+        pass
+
+    # no nonlocal keyword, so use a dictionary as a workaround
+    num_extra_attrs_calls = {"value": 0}
+
+    class MyCrumbler(Crumbler):
+
+        def get_extra_attrs_from_db(self, item_list):
+            # this is the main assert that makes sure greedy fetching works
+            # we create five parent/child/baby objects below, so this function
+            # should always be called with five items
+            assert len(item_list) == 5, "I didn't batch! item_count=%d" % len(
+                item_list)
+            num_extra_attrs_calls["value"] += 1
+
+            attrs = {}
+            for index, item in enumerate(item_list):
+                attrs[item] = (type(item).__name__, index)
+            return attrs
+
+        def crumble(self, item, attrs):
+            return {
+                "class": attrs[0],
+                "index": attrs[1],
+                "data": item.data,
+                "child": item.child if hasattr(item, "child") else None,
+                "baby": item.baby if hasattr(item, "baby") else None,
+            }
+
+    extended_registry = {
+        Parent: MyCrumbler(),
+        Child: MyCrumbler(),
+        Baby: MyCrumbler()
+    }
+
+    parents = []
+    for letter in ['a', 'b', 'c', 'd', 'e']:
+        child = Child(letter * 2)
+        child.baby = Baby(letter * 3)
+
+        parent = Parent(letter)
+        parent.child = child
+
+        parents.append(parent)
+
+    serialized = serialize(parents, extended_registry=extended_registry)
+    assert num_extra_attrs_calls["value"] == 3, "should have batched 3 different objects"
+
+    for index, parent in enumerate(serialized):
+        letter = ['a', 'b', 'c', 'd', 'e'][index]
+
+        assert parent["class"] == "Parent"
+        assert parent["index"] == index
+        assert parent["data"] == letter
+
+        child = parent["child"]
+        assert child["class"] == "Child"
+        assert child["index"] == index
+        assert child["data"] == letter * 2
+
+        baby = child["baby"]
+        assert baby["class"] == "Baby"
+        assert baby["index"] == index
+        assert baby["data"] == letter * 3
