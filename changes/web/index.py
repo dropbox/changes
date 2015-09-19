20e6cc527f29472bab574b54970cf5daf1e40f0e
Mihir Kedia <mkedia@dropbox.com>
2015-09-19 12:20:00 -0700

    ...and lint

--- changes/web/index.py
+++ changes/web/index.py
@@ -67,7 +67,7 @@ class IndexView(MethodView):
 
         if fetch_custom_js:
             IndexView.custom_js = open(current_app.config['WEBAPP_CUSTOM_JS']).read()
-        
+
         disable_custom = request.args and "disable_custom" in request.args
 
         # use new react code
@@ -76,11 +76,11 @@ class IndexView(MethodView):
                 'SENTRY_PUBLIC_DSN': dsn,
                 'RELEASE_INFO': changes.get_revision_info(),
                 'WEBAPP_USE_ANOTHER_HOST': use_another_host,
-                'WEBAPP_CUSTOM_JS': (IndexView.custom_js 
-                    if not disable_custom else None),
+                'WEBAPP_CUSTOM_JS': (IndexView.custom_js if
+                    not disable_custom else None),
                 'USE_PACKAGED_JS': not current_app.debug,
-                'HAS_CUSTOM_CSS': (current_app.config['WEBAPP_CUSTOM_CSS'] 
-                    and not disable_custom),
+                'HAS_CUSTOM_CSS': (current_app.config['WEBAPP_CUSTOM_CSS'] and
+                    not disable_custom),
                 'IS_DEBUG': current_app.debug,
                 'PHABRICATOR_HOST': current_app.config['PHABRICATOR_HOST']
             })
