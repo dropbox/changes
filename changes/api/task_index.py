5dc29f74ce4f72b3f0003c3fdfc818dbc93366a4
Kyle Consalus <kylec@dropbox.com>
2015-12-18 14:40:09 -0800

    Make it possible in the API to query for tasks by child_id (aka object id)
    Summary:
    This makes it much easier to build external tooling to inspect Task state.
    Prior to this, it could require 10s of thousands of requests to find the Task
    you're looking for.
    
    Test Plan: Used it locally.
    
    Reviewers: nate
    
    Reviewed By: nate
    
    Subscribers: changesbot
    
    Differential Revision: https://tails.corp.dropbox.com/D161628

--- changes/api/task_index.py
+++ changes/api/task_index.py
@@ -1,11 +1,24 @@
 from __future__ import absolute_import
 
+from flask_restful.reqparse import RequestParser
+
 from changes.api.base import APIView
-from changes.models import Task
+from changes.models.task import Task
+
+import uuid
 
 
 class TaskIndexAPIView(APIView):
+    get_parser = RequestParser()
+
+    # If specified, the results will be limited to Tasks associated with this object id.
+    get_parser.add_argument('object_id', type=uuid.UUID, default=None)
+
     def get(self):
+        args = self.get_parser.parse_args()
+
         queryset = Task.query.order_by(Task.date_created.desc())
+        if args.object_id:
+            queryset = queryset.filter(Task.task_id == args.object_id)
 
         return self.paginate(queryset)
