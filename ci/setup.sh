5632b1c97a7f168fe62b5c65945e5c46cf7b3c8b
Kyle Consalus <kylec@dropbox.com>
2016-01-11 14:58:20 -0800

    Install node/npm in ci/setup.sh
    Summary:
    We were relying on container setup to give us npm, but it recently stopped providing it,
    and that has broken our snapshotting. This makes us install npm ourselves.
    
    Test Plan: Tried it on Changes in many variations, all after an npm uninstall.
    
    Reviewers: paulruan
    
    Reviewed By: paulruan
    
    Subscribers: changesbot
    
    Differential Revision: https://tails.corp.dropbox.com/D164635

--- ci/setup.sh
+++ ci/setup.sh
@@ -1,7 +1,12 @@
 #!/bin/bash -ex
-sudo apt-get install -y build-essential python-setuptools redis-server postgresql libpq-dev libevent-dev libxml2-dev libxslt-dev libffi-dev
+sudo apt-get install -y build-essential python-setuptools redis-server postgresql libpq-dev libevent-dev libxml2-dev libxslt-dev libffi-dev nodejs
 
-npm --version
+# We don't necessarily need a specific version of npm, but this ensures we don't
+# use a particularly old one.
+NPM_VERSION='>= 3.5.2'
+# Only install if we can't list the expected version
+npm list -g npm@"$NPM_VERSION" || sudo npm install -g npm@"$NPM_VERSION"
+
 
 # Install bower only if it's not installed already.
 bower help || sudo npm install -g bower
