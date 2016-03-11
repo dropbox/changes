384b2edb6032eba9c0cc4647041908470b151b35
Mike Treaster <treaster@dropbox.com>
2016-03-11 10:59:25 -0800

    Implement a Changes API endpoint to provide aggregated jobstep status information to monitoring systems.
    Summary:
    Implement a Changes API endpoint to provide aggregated jobstep status
    information to monitoring systems.
    - Query the database for jobsteps with queued, pending_allocation, and
      in-progress status.
    - Generate aggregated stats by cluster, by project, and globally and return as
      JSON.
    
    Test Plan: Added unit test.
    
    Reviewers: kylec
    
    Reviewed By: kylec
    
    Subscribers: changesbot
    
    Differential Revision: https://tails.corp.dropbox.com/D179387

---
+++ changes/api/jobstep_aggregate_by_status.py
@@ -0,0 +1,68 @@
+from collections import defaultdict
+
+from changes.api.base import APIView
+from changes.constants import Status
+from changes.config import db
+from changes.models.jobstep import JobStep
+
+
+class JobStepAggregateByStatusAPIView(APIView):
+    def get(self):
+        """GET method that returns aggregated data regarding jobsteps.
+        Fetch pending, queued, and in-progress jobsteps from the database.
+        Compute some aggregate metrics about them.
+        Return the aggregated data in a JSON-friendly format.
+
+        Returns:
+            {
+                'jobsteps': {
+                    'by_cluster': {
+                        '[some cluster id]': [status data],
+                        ...
+                    },
+                    'by_project': {
+                        '[some project id]': [status data],
+                    },
+                    'global': [status_data],
+                },
+            }
+
+            where [status_data] is:
+            {
+                'status value': [ count of jobsteps in status,
+                                  oldest jobstep create time,
+                                  jobstep_id of oldest ],
+                ...
+            }
+        """
+        def process_row(agg, jobstep):
+            status = jobstep.status.name
+            count, date_created, jobstep_id = agg.get(status, (0, None, None))
+            count += 1
+
+            # Track the oldest jobstep we've seen.
+            if date_created is None or jobstep.date_created < date_created:
+                date_created = jobstep.date_created
+                jobstep_id = jobstep.id
+            agg[status] = (count, date_created, jobstep_id)
+
+        jobsteps = JobStep.query.filter(
+            JobStep.status.in_(
+                [Status.pending_allocation, Status.queued, Status.in_progress]),
+        )
+
+        by_cluster, by_project = defaultdict(dict), defaultdict(dict)
+        by_global = {}
+        for jobstep in jobsteps:
+            process_row(by_cluster[jobstep.cluster], jobstep)
+            process_row(by_project[jobstep.project_id], jobstep)
+            process_row(by_global, jobstep)
+
+        output = {
+            'jobsteps': {
+                'by_cluster': by_cluster,
+                'by_project': by_project,
+                'global': by_global,
+            },
+        }
+        return self.respond(output)
---
+++ tests/changes/api/test_jobstep_aggregate_by_status.py
@@ -0,0 +1,116 @@
+import datetime
+
+from urllib import urlencode
+
+from changes.testutils import APITestCase
+from changes.constants import Status
+
+
+class JobStepAggregateByStatusTest(APITestCase):
+    path = '/api/0/jobsteps/aggregate_by_status/'
+
+    def get(self, **kwargs):
+        query_string = '?' + urlencode(kwargs) if kwargs else ''
+        return self.client.get(self.path + query_string)
+
+    def test_get(self):
+        project_1 = self.create_project()
+        build_1 = self.create_build(project_1)
+        job_1 = self.create_job(build_1)
+        jobphase_1 = self.create_jobphase(job_1)
+
+        project_2 = self.create_project()
+        build_2 = self.create_build(project_2)
+        job_2 = self.create_job(build_2)
+        jobphase_2 = self.create_jobphase(job_2)
+
+        now = datetime.datetime.now()
+        jobstep_unknown = self.create_jobstep(
+            jobphase_1,
+            status=Status.unknown,
+            date_created=now,
+            cluster="cluster_a")
+        jobstep_queued = self.create_jobstep(
+            jobphase_2,
+            status=Status.queued,
+            date_created=now,
+            cluster="cluster_a")
+        jobstep_in_progress = self.create_jobstep(
+            jobphase_1,
+            status=Status.in_progress,
+            date_created=now,
+            cluster="cluster_a")
+        jobstep_finished = self.create_jobstep(
+            jobphase_2,
+            status=Status.finished,
+            date_created=now,
+            cluster="cluster_b")
+        jobstep_collecting_results = self.create_jobstep(
+            jobphase_1,
+            status=Status.collecting_results,
+            date_created=now,
+            cluster="cluster_b")
+        jobstep_allocated_ = self.create_jobstep(
+            jobphase_2,
+            status=Status.allocated,
+            date_created=now,
+            cluster="cluster_b")
+        jobstep_pending_allocation = self.create_jobstep(
+            jobphase_1,
+            status=Status.pending_allocation,
+            date_created=now,
+            cluster="cluster_c")
+
+        now_iso = now.isoformat()
+        expected_output = {
+            'jobsteps': {
+                'by_cluster': {
+                    "cluster_a": {
+                        Status.queued.name:
+                            [1, now_iso, jobstep_queued.id.get_hex()],
+                        Status.in_progress.name:
+                            [1, now_iso, jobstep_in_progress.id.get_hex()],
+                    },
+                    "cluster_c": {
+                        Status.pending_allocation.name:
+                            [1, now_iso, jobstep_pending_allocation.id.get_hex()],
+                    },
+                },
+                'by_project': {
+                    project_1.id.get_hex(): {
+                        Status.in_progress.name:
+                            [1, now_iso, jobstep_in_progress.id.get_hex()],
+                        Status.pending_allocation.name:
+                            [1, now_iso, jobstep_pending_allocation.id.get_hex()],
+                    },
+                    project_2.id.get_hex(): {
+                        Status.queued.name:
+                            [1, now_iso, jobstep_queued.id.get_hex()],
+                    },
+                },
+                'global': {
+                    Status.queued.name:
+                        [1, now_iso, jobstep_queued.id.get_hex()],
+                    Status.in_progress.name:
+                        [1, now_iso, jobstep_in_progress.id.get_hex()],
+                    Status.pending_allocation.name:
+                        [1, now_iso, jobstep_pending_allocation.id.get_hex()],
+                },
+            }
+        }
+
+        # Execute the request..
+        raw_resp = self.get()
+        assert raw_resp.status_code == 200
+        response_data = self.unserialize(raw_resp)
+
+        # Check some keys individually, to make debugging/diffing easier if
+        # there's a problem.
+        expected_jobsteps = expected_output['jobsteps']
+        received_jobsteps = response_data['jobsteps']
+        for key, expected_value in expected_jobsteps.iteritems():
+            assert received_jobsteps[key] == expected_value
+
+        # But also check everything all together, to catch any errors that
+        # slipped through the previous not-quite-rigorous asserts.
+        assert response_data == expected_output
