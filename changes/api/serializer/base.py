6826acae9c76e1736813911a04b6742a196850bb
Nathaniel Herman <nate@dropbox.com>
2016-03-31 18:08:07 -0700

    Batch get_extra_attrs_from_db in the serializer as much as possible.
    Summary:
    - Based off of an earlier implementation from Mihir.
    - I'm still tempted to just rewrite the logic in a way that makes more sense to me (though I'm not totally sure what that way would be), but I'm reasonably convinced this will work from the test coverage.
    
    Test Plan: Added several unit tests.
    
    Reviewers: josiah, kylec
    
    Reviewed By: kylec
    
    Subscribers: nate, changesbot, kylec
    
    Differential Revision: https://tails.corp.dropbox.com/D142690

--- changes/api/serializer/base.py
+++ changes/api/serializer/base.py
@@ -1,25 +1,127 @@
+from collections import defaultdict
 from datetime import datetime
 from enum import Enum
 from uuid import UUID
+from typing import cast, Any, Callable, Dict, Generic, Iterator, List, Optional, TypeVar, Tuple, Union  # NOQA
 
+from flask import request
 
-# Types for which serialization is a no-op.
+# Types for which serialization is a no-op. Everything boils down to these (or
+# to objects without crumblers, which we pass through unscathed)
 _PASSTHROUGH = (basestring, bool, int, long, type(None), float)
 
+T = TypeVar('T')
 
-def serialize(data, extended_registry=None, use_greedy=False):
-    """
-    Converts a data structure of dicts, lists, SQLAlchemy objects, and other
-    random python objects into something that can be passed to JSON.dumps. This
-    is not a guarantee...if data contains an object that we don't know how to
-    handle, we'll just leave it in.
 
-    extended_registry: additional Crumblers to use. e.g. one API might
-    want to use a special crumbler for Jobs that also adds Build
-    information and passes { Job: JobWithBuildCrumbler }
+class SerializeTask(Generic[T]):
+    class State(Enum):
+        # states
+        uncalled = 0  # this task hasn't yet been called
+        done = 1  # this task is done. Call get() for the result
+        depends_on = 2  # finish a list of tasks first, then resume me
+        fetch_for = 3  # fetch this data, then send it back to me
+        fetched = 4  # the scheduler fetched our data. continue!
 
-    Its safe (but CPU-expensive) to rerun serialize on data multiple times
-    """
+    def __init__(self, data):
+        # type: (T) -> None
+        self.data = data
+        self.state = SerializeTask.State.uncalled
+        self.result = None  # type: Optional[object]
+
+        self._generator = None  # type: Optional[Iterator[Tuple[SerializeTask.State, Optional[List[SerializeTask[object]]]]]]
+
+    def _run(self):
+        # type: () -> Iterator[Tuple[SerializeTask.State, Optional[List[SerializeTask[object]]]]]
+        if isinstance(self.data, _PASSTHROUGH):
+            yield self._done(self.data)
+
+        elif isinstance(self.data, (list, tuple, set, frozenset)):
+            data = cast(Union[list, tuple, set, frozenset], self.data)
+            if not isinstance(data, list):
+                data = list(data)
+
+            serializer_tasks = {}  # type: Dict[int, SerializeTask[object]]
+            for index, item in enumerate(data):
+                if isinstance(item, _PASSTHROUGH):
+                    continue
+                serializer_tasks[index] = SerializeTask(item)
+
+            yield self._depends_on(serializer_tasks.values())
+            result = []  # type: List[object]
+            for index, item in enumerate(data):
+                if index in serializer_tasks:
+                    result.append(serializer_tasks[index].get())
+                else:
+                    result.append(item)
+            yield self._done(result)
+
+        elif isinstance(self.data, dict):
+            key_serialize_task = SerializeTask(self.data.keys())
+            value_serialize_task = SerializeTask(self.data.values())
+
+            yield self._depends_on([key_serialize_task, value_serialize_task])
+            yield self._done(dict(zip(key_serialize_task.get(), value_serialize_task.get())))
+
+        else:
+            yield self._fetch_for(self.data)
+            assert self.state == SerializeTask.State.fetched
+            if isinstance(self.fetched, _PASSTHROUGH):
+                yield self._done(self.fetched)
+                return
+            # result might still have dependencies
+            serializer_task = SerializeTask(self.fetched)
+            yield self._depends_on([serializer_task])
+            yield self._done(serializer_task.get())
+
+    def generator(self):
+        # type: () -> Iterator[Tuple[SerializeTask.State, Optional[List[SerializeTask[object]]]]]
+        if not self._generator:
+            self._generator = self._run()
+        return self._generator
+
+    def get(self):
+        # type: () -> object
+        return self.result
+
+    def _done(self, result):
+        # type: (object) -> Tuple[SerializeTask.State, Optional[List[SerializeTask[object]]]]
+        self.state = SerializeTask.State.done
+        self.result = result
+        return (self.state, None)
+
+    def _depends_on(self, serialize_task_list):
+        # type: (List[SerializeTask]) -> Tuple[SerializeTask.State, List[SerializeTask[object]]]
+        self.state = SerializeTask.State.depends_on
+        self.dependents = serialize_task_list
+        return (self.state, serialize_task_list)
+
+    def _dependents_are_ready(self):
+        # type: () -> bool
+        return all([task.state == SerializeTask.State.done for task in self.dependents])
+
+    def _fetch_for(self, data):
+        # type: (T) -> Tuple[SerializeTask.State, Optional[List[SerializeTask[object]]]]
+        self.state = SerializeTask.State.fetch_for
+        self.fetchable = data
+        return (self.state, None)
+
+    def runnable(self):
+        # type: () -> bool
+        return (self.state in (SerializeTask.State.uncalled, SerializeTask.State.fetched) or
+                (self.state == SerializeTask.State.depends_on and
+                 self._dependents_are_ready()))
+
+    def get_fetchable(self):
+        # type: () -> object
+        return self.fetchable
+
+    def put_fetched(self, fetched):
+        # type: (object) -> None
+        self.state = SerializeTask.State.fetched
+        self.fetched = fetched
+
+
+def old_serialize(data, extended_registry=None):
     if extended_registry is None:
         extended_registry = {}
 
@@ -30,8 +132,8 @@ def serialize(data, extended_registry=None, use_greedy=False):
         for k, v in data.iteritems():
             if not isinstance(v, _PASSTHROUGH) or not isinstance(k, _PASSTHROUGH):
                 # Gotta do it the hard way.
-                return dict(zip(serialize(data.keys(), extended_registry),
-                                serialize(data.values(), extended_registry)))
+                return dict(zip(old_serialize(data.keys(), extended_registry),
+                                old_serialize(data.values(), extended_registry)))
         # All keys and values were passthrough, so the dict is already
         # serialized.
         return data
@@ -57,7 +159,7 @@ def serialize(data, extended_registry=None, use_greedy=False):
                 attrs = crumbler.get_extra_attrs_from_db(data)
                 data = [crumbler(o, attrs=attrs.get(o)) for o in data]
 
-        return [serialize(j, extended_registry) for j in data]
+        return [old_serialize(j, extended_registry) for j in data]
 
     # if we're here, we have a single object that we probably need to convert
     # using a crumbler
@@ -69,7 +171,74 @@ def serialize(data, extended_registry=None, use_greedy=False):
     attrs = crumbler.get_extra_attrs_from_db([data])
     data = crumbler(data, attrs=attrs.get(data))
 
-    return serialize(data, extended_registry)
+    return old_serialize(data, extended_registry)
+
+
+def new_serialize(data, extended_registry=None):
+    # type: (object, Optional[Dict[type, Crumbler[object]]]) -> object
+    initial_task = SerializeTask(data)
+    all_tasks = [initial_task]
+
+    while initial_task.state != SerializeTask.State.done:
+        # our main loop. Basically:
+        # - ignore done tasks
+        # - if we have any uncalled tasks, tasks that have data ready to use,
+        #   or depends_on tasks where all dependents are done, run all of
+        #   those tasks for this loop
+        # - otherwise we must be stuck on fetch_fors. Do all of the data
+        #   fetching
+        tasks_to_run = [task for task in all_tasks if task.runnable()]
+
+        if tasks_to_run:
+            for task in tasks_to_run:
+                result = next(task.generator(), None)
+                if result and result[0] == SerializeTask.State.depends_on:
+                    all_tasks.extend(result[1])
+            continue
+
+        # ok, we must be stuck on fetch_fors. Do all of them
+        fetch_tasks = [task for task in all_tasks if task.state == SerializeTask.State.fetch_for]
+        fetches_by_class = defaultdict(list)  # type: Dict[type, List[SerializeTask[object]]]
+
+        for f in fetch_tasks:
+            fetches_by_class[f.get_fetchable().__class__].append(f)
+
+        for cls, tasks in fetches_by_class.iteritems():
+            # we can use the same crumbler object for all items of this type.
+            crumbler = get_crumbler(tasks[0].get_fetchable(), extended_registry)
+            if crumbler is None:
+                # no crumbler for these objects, so just output them directly
+                for task in tasks:
+                    task.put_fetched(task.get_fetchable())
+                continue
+            extra_attrs = crumbler.get_extra_attrs_from_db(
+                {task.get_fetchable() for task in tasks})
+            for task in tasks:
+                item = task.get_fetchable()
+                task.put_fetched(
+                    crumbler.crumble(item, extra_attrs.get(item)))
+
+    return initial_task.get()
+
+
+def serialize(data, extended_registry=None):
+    # type: (object, Optional[Dict[type, Crumbler[object]]]) -> object
+    """
+    Converts a data structure of dicts, lists, SQLAlchemy objects, and other
+    random python objects into something that can be passed to JSON.dumps. This
+    is not a guarantee...if data contains an object that we don't know how to
+    handle, we'll just leave it in.
+
+    extended_registry: additional Crumblers to use. e.g. one API might
+    want to use a special crumbler for Jobs that also adds Build
+    information and passes { Job: JobWithBuildCrumbler }
+
+    Its safe (but CPU-expensive) to rerun serialize on data multiple times
+    """
+    if request.args.get('__new_serialize__') == '1':
+        return new_serialize(data, extended_registry)
+    else:
+        return old_serialize(data, extended_registry)
 
 
 #
@@ -81,10 +250,11 @@ def serialize(data, extended_registry=None, use_greedy=False):
 # class to use and calls it on its objects
 #
 
-_registry = {}
+_registry = {}  # type: Dict[type, Crumbler[object]]
 
 
 def register(type):
+    # type: (type) -> Callable[[type], type]
     def wrapped(cls):
         _registry[type] = cls()
         return cls
@@ -92,20 +262,28 @@ def register(type):
 
 
 def get_crumbler(item, registry):
-    item_type = type(item)
+    # type: (T, Optional[Dict[type, Crumbler[object]]]) -> Crumbler[T]
+    item_type = type(item)  # type: ignore
 
-    crumbler = registry.get(item_type, _registry.get(item_type))
+    crumbler = None
+    if registry:
+        crumbler = registry.get(item_type)
+    if crumbler is None:
+        crumbler = _registry.get(item_type)
 
     if crumbler is None:
         for cls, _crumbler in _registry.iteritems():
+            # XXX(nate): issubclass doesn't seem that safe. We aren't iterating
+            # in a fixed order.
             if issubclass(item_type, cls):
                 crumbler = _crumbler
                 break
 
-    return crumbler
+    return cast(Crumbler[T], crumbler)
 
 
-class Crumbler(object):
+# TODO(nate): these methods should all just be made static.
+class Crumbler(Generic[T]):
     """
     Converts an object (most often a SQLAlchemy object) to a dict/string/int.
     This is shallow: the returned dict may have values that need to be crumbled
@@ -116,19 +294,22 @@ class Crumbler(object):
     """
 
     def __call__(self, item, attrs):
+        # type: (T, Dict[str, Any]) -> object
         return self.crumble(item, attrs)
 
-    def get_extra_attrs_from_db(self, item_list):
+    def get_extra_attrs_from_db(self, item_set):
+        # type: (Set[T]) -> Dict[T, Dict[str, Any]]
         """
         We may need to do additional data fetching to convert an object: for
         example, we want to look up the phabricator callsign when returning
-        revision objects. This function can take a list of objects to crumble
-        and returns an attrs dict (object => additional fetched data to use in
-        crumble)
+        revision objects. This function can take a set of objects to crumble
+        and returns an attrs dict (object => dict of fetched data for use
+        in `crumble`)
         """
         return {}
 
     def crumble(self, item, attrs):
+        # type: (T, Dict[str, Any]) -> object
         """
         Does the actual conversion from object to something simpler. attrs
         should come from get_extra_attrs_from_db, e.g.:
--- tests/changes/api/serializer/test_base.py
+++ tests/changes/api/serializer/test_base.py
@@ -1,16 +1,152 @@
-from changes.api.serializer import serialize
+import mock
+
+from changes.api.serializer import Crumbler, new_serialize as serialize
+from changes.testutils import TestCase
 
 
-def test_identity():
-    """Verify that serialize returns what we gave it in basic cases."""
-    passthrough = [
-        None,
-        44.2,
-        "string",
-        [1, 2, 3],
-        ["one", 2, 3],
-        {'x': 11, 'y': 22},
-        {'x': ['yes', 'no'], 'y': {'k': [1, 2, 3]}}
-    ]
-    for val in passthrough:
-        assert serialize(val) == val
+class SerializeTest(TestCase):
+    def test_identity(self):
+        """Verify that serialize returns what we gave it in basic cases."""
+        passthrough = [
+            None,
+            44.2,
+            "string",
+            [1, 2, 3],
+            ["one", 2, 3],
+            {'x': 11, 'y': 22},
+            {'x': ['yes', 'no'], 'y': {'k': [1, 2, 3]}}
+        ]
+        for val in passthrough:
+            assert serialize(val) == val
+
+    class _Foo(object):
+        def __init__(self, inner):
+            self.inner = inner
+
+    class _Bar(object):
+        def __init__(self, inner):
+            self.inner = inner
+
+    def _setup_crumblers(self, get_crumbler):
+        """
+        Given a mocked get_crumbler, sets it up with crumblers for the _Foo
+        and _Bar classes.
+        Returns:
+            tuple of (foo_crumbler, bar_crumbler)
+        """
+        foo_crumbler = mock.Mock(spec=Crumbler())
+        foo_crumbler.crumble.side_effect = lambda item, attrs: item.inner
+        foo_crumbler.get_extra_attrs_from_db.return_value = {}
+        bar_crumbler = mock.Mock(spec=Crumbler())
+        bar_crumbler.crumble.side_effect = lambda item, attrs: item.inner
+        bar_crumbler.get_extra_attrs_from_db.return_value = {}
+        crumbler_mapping = {SerializeTest._Foo: foo_crumbler, SerializeTest._Bar: bar_crumbler}
+        get_crumbler.side_effect = lambda item, registry: crumbler_mapping[item.__class__]
+
+        return (foo_crumbler, bar_crumbler)
+
+    def _assert_crumble_called_for(self, crumbler, items, any_order=False, attrs=None):
+        """
+        Asserts that crumbler.crumble() was called on exactly the given list of items, and no others.
+        Args:
+            crumbler: the mocked crumbler in question.
+            items: list of objects, each of which we expect a crumble() call for.
+            any_order: if False, items is assumed to be in the expected order of calls.
+            attrs: expected extra attributes argument to crumble (default None)
+        """
+        crumbler.crumble.assert_has_calls([mock.call(item, attrs)
+                                           for item in items], any_order=any_order)
+        assert crumbler.crumble.call_count == len(items)
+
+    @mock.patch('changes.api.serializer.base.get_crumbler')
+    def test_list(self, get_crumbler):
+        foo_crumbler, bar_crumbler = self._setup_crumblers(get_crumbler)
+
+        item1 = SerializeTest._Foo('foo')
+        item2 = SerializeTest._Bar('bar')
+        item3 = SerializeTest._Foo('foo2')
+        ret = serialize([item1, item2, item3, 'passthrough'])
+        assert ret == ['foo', 'bar', 'foo2', 'passthrough']
+        foo_crumbler.get_extra_attrs_from_db.assert_called_once_with({item1, item3})
+        bar_crumbler.get_extra_attrs_from_db.assert_called_once_with({item2})
+
+        self._assert_crumble_called_for(foo_crumbler, [item1, item3])
+        self._assert_crumble_called_for(bar_crumbler, [item2])
+
+    @mock.patch('changes.api.serializer.base.get_crumbler')
+    def test_dict(self, get_crumbler):
+        foo_crumbler, bar_crumbler = self._setup_crumblers(get_crumbler)
+
+        item1 = SerializeTest._Foo('foo')
+        item2 = SerializeTest._Bar('bar')
+        item3 = SerializeTest._Foo('foo2')
+        item4 = SerializeTest._Bar('baz')
+        ret = serialize({item1: item2, 'otherkey': item3, item4: 'otherval'})
+        assert ret == {'foo': 'bar', 'otherkey': 'foo2', 'baz': 'otherval'}
+        foo_crumbler.get_extra_attrs_from_db.assert_called_once_with({item1, item3})
+        bar_crumbler.get_extra_attrs_from_db.assert_called_once_with({item4, item2})
+
+        # we parse keys first, then values so there are no issues with dict ordering here
+        self._assert_crumble_called_for(foo_crumbler, [item1, item3])
+        self._assert_crumble_called_for(bar_crumbler, [item4, item2])
+
+    @mock.patch('changes.api.serializer.base.get_crumbler')
+    def test_recursive(self, get_crumbler):
+        foo_crumbler, bar_crumbler = self._setup_crumblers(get_crumbler)
+
+        item1 = SerializeTest._Bar(['baz'])
+        item2 = SerializeTest._Foo(item1)
+        item3 = SerializeTest._Foo({'foo': ['thing', item2, 'other'],
+                                   'otherkey': 'otherval'})
+        item4 = SerializeTest._Bar('bar')
+        ret = serialize([item3, item4])
+        assert ret == [{'foo': ['thing', ['baz'], 'other'], 'otherkey': 'otherval'}, 'bar']
+        # We won't be able to see item2 until we've unwrapped item3, so in this
+        # case, we do expect separate calls to `get_extra_attrs_from_db`.
+        foo_crumbler.get_extra_attrs_from_db.assert_has_calls([mock.call({item3}),
+                                                               mock.call({item2})])
+        bar_crumbler.get_extra_attrs_from_db.assert_has_calls([mock.call({item4}),
+                                                               mock.call({item1})])
+
+        self._assert_crumble_called_for(foo_crumbler, [item3, item2])
+        self._assert_crumble_called_for(bar_crumbler, [item4, item1])
+
+    @mock.patch('changes.api.serializer.base.get_crumbler')
+    def test_embedded(self, get_crumbler):
+        foo_crumbler, bar_crumbler = self._setup_crumblers(get_crumbler)
+
+        item1 = SerializeTest._Bar('bar')
+        item2 = SerializeTest._Foo({'foo': item1})
+        item3 = SerializeTest._Bar('baz')
+        item4 = SerializeTest._Foo({'fuzz': item3})
+        ret = serialize([item2, item4])
+        assert ret == [{'foo': 'bar'}, {'fuzz': 'baz'}]
+        foo_crumbler.get_extra_attrs_from_db.assert_called_once_with({item2, item4})
+        bar_crumbler.get_extra_attrs_from_db.assert_called_once_with({item1, item3})
+
+        self._assert_crumble_called_for(foo_crumbler, [item2, item4])
+        self._assert_crumble_called_for(bar_crumbler, [item1, item3])
+
+    @mock.patch('changes.api.serializer.base.get_crumbler')
+    def test_embedded_get_extra_attrs(self, get_crumbler):
+        foo_crumbler, bar_crumbler = self._setup_crumblers(get_crumbler)
+
+        item1 = SerializeTest._Bar('bar')
+        item2 = SerializeTest._Foo('foo')
+        item3 = SerializeTest._Foo('fuzz')
+
+        foo_crumbler.crumble.side_effect = lambda item, attrs: [item.inner, attrs['foo']]
+        attrs = {'foo': item1}
+        foo_crumbler.get_extra_attrs_from_db.return_value = {item2: attrs,
+                                                             item3: attrs}
+
+        ret = serialize([item2, item3])
+        assert ret == [['foo', 'bar'], ['fuzz', 'bar']]
+        foo_crumbler.get_extra_attrs_from_db.assert_called_once_with({item2, item3})
+        bar_crumbler.get_extra_attrs_from_db.assert_called_once_with({item1})
+
+        self._assert_crumble_called_for(foo_crumbler, [item2, item3], attrs=attrs)
+        # we don't currently batch crumble() calls for the same object, so
+        # crumble() will actually get called twice here. The assumption is that
+        # crumble() itself is cheap, while get_extra_attrs_from_db() is not.
+        self._assert_crumble_called_for(bar_crumbler, [item1, item1])
