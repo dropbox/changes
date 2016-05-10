2eeb422fcc90a7cee97f1175ba7970facbda6429
Kyle Consalus <kylec@dropbox.com>
2016-05-10 13:03:36 -0700

    Require that webapp/entry.js be ES5
    Summary:
    Failure is communicated by a junit xml.
    Could avoid blocking the rest of the build, but "won't work in browsers" seems worth stopping for.
    
    Test Plan: https://changes.build.itc.dropbox.com/single_build/bef5f4ad83fc4f308111bdbd0d9c97d1/
    
    Reviewers: nate
    
    Reviewed By: nate
    
    Subscribers: changesbot
    
    Differential Revision: https://tails.corp.dropbox.com/D195028

--- ci/run_tests.sh
+++ ci/run_tests.sh
@@ -1,4 +1,6 @@
 #!/bin/bash -ex
 
 source ~/env/bin/activate
+# webapp/entry.js must be ES5-compatible; we don't currently preprocess it.
+node_modules/.bin/eslint --no-eslintrc --parser-options=ecmaVersion:5 -f junit webapp/entry.js > entry_eslint.junit.xml
 make test-full
