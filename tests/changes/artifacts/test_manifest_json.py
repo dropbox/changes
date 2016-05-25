3b2297e5a8d89231969524bec612726282412c13
Nathaniel Herman <nate@dropbox.com>
2016-05-25 10:00:22 -0700

    Re-enable disabled manifest json tests
    Summary: - These should pass now
    
    Test Plan: CI
    
    Reviewers: kylec
    
    Reviewed By: kylec
    
    Subscribers: changesbot
    
    Differential Revision: https://tails.corp.dropbox.com/D199375

--- tests/changes/artifacts/test_manifest_json.py
+++ tests/changes/artifacts/test_manifest_json.py
@@ -29,15 +29,13 @@ class ManifestJsonHandlerTest(TestCase):
         jobstep = self.create_jobstep(jobphase)
         handler = ManifestJsonHandler(jobstep)
 
-        # TODO(nate): temporarily disabled
-        if False:
-            fp = StringIO(self.json_file_format % '1')
-            handler.process(fp)
-            assert jobstep.result == Result.infra_failed
-            assert FailureReason.query.filter(
-                FailureReason.step_id == jobstep.id,
-                FailureReason.reason == 'malformed_manifest_json',
-            ).first()
+        fp = StringIO(self.json_file_format % '1')
+        handler.process(fp)
+        assert jobstep.result == Result.infra_failed
+        assert FailureReason.query.filter(
+            FailureReason.step_id == jobstep.id,
+            FailureReason.reason == 'malformed_manifest_json',
+        ).first()
 
     def test_malformed(self):
         project = self.create_project()
@@ -47,12 +45,10 @@ class ManifestJsonHandlerTest(TestCase):
         jobstep = self.create_jobstep(jobphase)
         handler = ManifestJsonHandler(jobstep)
 
-        # TODO(nate): temporarily disabled
-        if False:
-            fp = StringIO('invalid_file')
-            handler.process(fp)
-            assert Result.infra_failed
-            assert FailureReason.query.filter(
-                FailureReason.step_id == jobstep.id,
-                FailureReason.reason == 'malformed_manifest_json',
-            ).first()
+        fp = StringIO('invalid_file')
+        handler.process(fp)
+        assert Result.infra_failed
+        assert FailureReason.query.filter(
+            FailureReason.step_id == jobstep.id,
+            FailureReason.reason == 'malformed_manifest_json',
+        ).first()
