4bfeac1113686f9d7b6c4d3b4f1904520001bafa
Naphat Sanguansin <naphat@dropbox.com>
2016-09-27 16:35:51 -0700

    properly handle a bazel target with no jobstep in serializer
    Reviewers: anupc
    
    Reviewed By: anupc
    
    Subscribers: changesbot, kylec, wwu
    
    Differential Revision: https://tails.corp.dropbox.com/D232458

--- changes/api/serializer/models/bazeltarget.py
+++ changes/api/serializer/models/bazeltarget.py
@@ -5,13 +5,15 @@ from changes.models.bazeltarget import BazelTarget
 @register(BazelTarget)
 class BazelTargetCrumbler(Crumbler):
     def crumble(self, instance, attrs):
-        return {
+        result = {
             'id': instance.id.hex,
             'job': {'id': instance.job_id.hex},
-            'step': {'id': instance.step_id.hex},
             'name': instance.name,
             'duration': instance.duration or 0,
             'status': instance.status,
             'result': instance.result,
             'dateCreated': instance.date_created,
         }
+        if instance.step_id:
+            result['step'] = {'id': instance.step_id.hex}
+        return result
--- tests/changes/api/serializer/models/test_bazeltarget.py
+++ tests/changes/api/serializer/models/test_bazeltarget.py
@@ -22,6 +22,29 @@ class BazelTargetCrumblerTestCase(TestCase):
         result = serialize(target)
         assert result['id'] == str(target.id.hex)
         assert result['job']['id'] == str(job.id.hex)
+        assert result['step']['id'] == str(step.id.hex)
+        assert result['name'] == 'target_foo'
+        assert result['dateCreated'] == '2013-09-19T22:15:22'
+        assert result['result']['id'] == 'failed'
+        assert result['status']['id'] == 'finished'
+        assert result['duration'] == 134
+
+    def test_no_step(self):
+        project = self.create_project()
+        build = self.create_build(project=project)
+        job = self.create_job(build=build)
+        phase = self.create_jobphase(job)
+        target = self.create_target(job,
+            name='target_foo',
+            duration=134,
+            result=Result.failed,
+            status=Status.finished,
+            date_created=datetime(2013, 9, 19, 22, 15, 22),
+        )
+        result = serialize(target)
+        assert result['id'] == str(target.id.hex)
+        assert result['job']['id'] == str(job.id.hex)
+        assert 'step' not in result
         assert result['name'] == 'target_foo'
         assert result['dateCreated'] == '2013-09-19T22:15:22'
         assert result['result']['id'] == 'failed'
